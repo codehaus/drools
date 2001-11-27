252336ecf96b65e6712da8a987d9dc98e9dfee62
werken <werken@e9a468ab-b372-0410-861a-d954c37f4f16>
2001-11-27 19:13:46 +0000

    Testing AssignmentNode

--- trunk/drools/drools-core/src/test/org/drools/reteoo/AssignmentNodeTest.java
+++ trunk/drools/drools-core/src/test/org/drools/reteoo/AssignmentNodeTest.java
@@ -1,12 +1,17 @@
 
 package org.drools.reteoo;
 
+import org.drools.AssertionException;
 import org.drools.spi.Declaration;
+import org.drools.spi.Tuple;
 import org.drools.semantic.java.JavaObjectType;
+
+import org.drools.spi.InstrumentedFactExtractor;
 
 import junit.framework.TestCase;
 
 import java.util.Set;
+import java.util.List;
 
 public class AssignmentNodeTest extends TestCase
 {
@@ -48,6 +53,54 @@ public class AssignmentNodeTest extends TestCase
 
         assertTrue( decls.contains( objectDecl ) );
         assertTrue( decls.contains( stringDecl ) );
+    }
+
+    public void testAssertTuple()
+    {
+        Declaration stringDecl = new Declaration( new JavaObjectType( String.class ),
+                                                  "string" );
+
+        Declaration objectDecl = new Declaration( new JavaObjectType( Object.class),
+                                                  "object" );
+
+        MockTupleSource source = new MockTupleSource();
+
+        source.addTupleDeclaration( objectDecl );
+
+        AssignmentNode assignNode = new AssignmentNode( source,
+                                                        stringDecl,
+                                                        new InstrumentedFactExtractor( "cheese" ) );
+
+        InstrumentedTupleSink sink = new InstrumentedTupleSink();
+
+        assignNode.setTupleSink( sink );
+
+        ReteTuple tuple = new ReteTuple();
+
+        try
+        {
+            assignNode.assertTuple( source,
+                                    tuple,
+                                    null );
+            
+            List assertedTuples = sink.getAssertedTuples();
+            
+            assertEquals( 1,
+                          assertedTuples.size() );
+            
+            Tuple assertedTuple = (Tuple) assertedTuples.get( 0 );
+            
+            Object value = assertedTuple.get( stringDecl );
+            
+            assertNotNull( value );
+            
+            assertEquals( "cheese",
+                          value );
+        }
+        catch (AssertionException e)
+        {
+            fail( e.toString() );
+        }
     }
 }
 
