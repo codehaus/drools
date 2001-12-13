04af74af05d51196e100085af7c8243ff54dfdc5
werken <werken@e9a468ab-b372-0410-861a-d954c37f4f16>
2001-12-13 21:18:00 +0000

    too many slashes

--- drools/drools-examples/deploy-web.sh
+++ drools/drools-examples/deploy-web.sh
@@ -8,4 +8,4 @@ ssh werken@drools.org mv /home/groups/d/dr/drools/htdocs /home/groups/d/dr/drool
 	&& \
 ssh werken@drools.org 'cd /home/groups/d/dr/drools && tar zxvf drools-web.tar.gz' \
 	&& \
-ssh werken@drools.org 'cd /home/groups/d/dr/drools && rm drools-web.tar.gz' \
+ssh werken@drools.org 'cd /home/groups/d/dr/drools && rm drools-web.tar.gz' 
