5c17db58bc6900566700336d856ab34fd9894641
werken <werken@e9a468ab-b372-0410-861a-d954c37f4f16>
2001-12-13 21:14:55 +0000

    Fixing some errors

--- trunk/drools/drools-core/deploy-web.sh
+++ trunk/drools/drools-core/deploy-web.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-./build.pl web.stage
-scp ./build/deploy/drools-web.tar.gz werken@drools.org:/home/groups/d/dr/drools
-ssh werken@drools.org mv /home/groups/d/dr/drools/htdocs /home/groups/d/dr/drools/htdocs.old
-ssh werken@drools.org 'cd /home/groups/d/dr/drools && tar zxvf drools-web.tar.gz'
-ssh werken@drools.org 'cd /home/groups/d/dr/drools && rm drools-web.tar.gz'
+./build.pl web.stage \
+	&& \
+scp ./build/deploy/drools-web.tar.gz werken@drools.org:/home/groups/d/dr/drools \
+	&& \
+ssh werken@drools.org mv /home/groups/d/dr/drools/htdocs /home/groups/d/dr/drools/htdocs.old \
+	&& \
+ssh werken@drools.org 'cd /home/groups/d/dr/drools && tar zxvf drools-web.tar.gz' \
+	&& \
+ssh werken@drools.org 'cd /home/groups/d/dr/drools && rm drools-web.tar.gz' \
