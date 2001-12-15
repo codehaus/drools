68fc5bace6b48c5b2693afe981d8175efcdd798e
werken <werken@e9a468ab-b372-0410-861a-d954c37f4f16>
2001-12-15 14:11:00 +0000

    Fixing my nightly-build script more.

--- trunk/drools/drools-examples/deploy-web.sh
+++ trunk/drools/drools-examples/deploy-web.sh
@@ -4,6 +4,8 @@
 	&& \
 scp ./build/deploy/drools-web.tar.gz werken@drools.org:/home/groups/d/dr/drools \
 	&& \
+ssh werken@drools.org rm -Rf /home/groups/d/dr/drools/htdocs.old \
+	&& \
 ssh werken@drools.org mv /home/groups/d/dr/drools/htdocs /home/groups/d/dr/drools/htdocs.old \
 	&& \
 ssh werken@drools.org 'cd /home/groups/d/dr/drools && tar zxvf drools-web.tar.gz' \
