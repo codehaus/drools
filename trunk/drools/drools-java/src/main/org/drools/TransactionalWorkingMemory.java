dfb2c1cf41ba4722841cf1194789edd56acf4b3f
bob <bob@e9a468ab-b372-0410-861a-d954c37f4f16>
2002-07-26 19:41:06 +0000

    Round one of cleanup/documentation/checkstyle-fixes.

--- trunk/drools/drools-java/src/main/org/drools/TransactionalWorkingMemory.java
+++ trunk/drools/drools-java/src/main/org/drools/TransactionalWorkingMemory.java
@@ -1,5 +1,50 @@
-
 package org.drools;
+
+/*
+ $Id: TransactionalWorkingMemory.java,v 1.4 2002-07-26 19:41:05 bob Exp $
+
+ Copyright 2002 (C) The Werken Company. All Rights Reserved.
+ 
+ Redistribution and use of this software and associated documentation
+ ("Software"), with or without modification, are permitted provided
+ that the following conditions are met:
+
+ 1. Redistributions of source code must retain copyright
+    statements and notices.  Redistributions must also contain a
+    copy of this document.
+ 
+ 2. Redistributions in binary form must reproduce the
+    above copyright notice, this list of conditions and the
+    following disclaimer in the documentation and/or other
+    materials provided with the distribution.
+ 
+ 3. The name "drools" must not be used to endorse or promote
+    products derived from this Software without prior written
+    permission of The Werken Company.  For written permission,
+    please contact bob@werken.com.
+ 
+ 4. Products derived from this Software may not be called "drools"
+    nor may "drools" appear in their names without prior written
+    permission of The Werken Company. "drools" is a registered
+    trademark of The Werken Company.
+ 
+ 5. Due credit should be given to The Werken Company.
+    (http://drools.werken.com/).
+ 
+ THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS
+ ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
+ NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ THE WERKEN COMPANY OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ */
 
 import java.util.Set;
 import java.util.HashSet;
@@ -13,15 +58,23 @@ import java.util.Iterator;
  *  rule action invokation upon {@link #commit}.  
  *  </p>
  *
- *  @author <a href="mailto:bob@werken.com">bob@werken.com</a>
+ *  @author <a href="mailto:bob@werken.com">bob mcwhirter</a>
  */
 public class TransactionalWorkingMemory extends WorkingMemory
 {
+    // ------------------------------------------------------------
+    //     Instance members
+    // ------------------------------------------------------------
+
     /** Object to assert upon commit. */
     private Set     assertedObjects;
 
     /** Objects retracted during commit. */
     private Set     retractedObjects;
+
+    // ------------------------------------------------------------
+    //     Constructors
+    // ------------------------------------------------------------
 
     /** Construct.
      *
@@ -34,49 +87,16 @@ public class TransactionalWorkingMemory extends WorkingMemory
         this.assertedObjects = new HashSet();
     }
 
-    public void assertObject(Object object) throws AssertionException
-    {
-        if ( this.retractedObjects != null )
-        {
-            super.assertObject( object );
-        }
-        else
-        {
-            this.assertedObjects.add( object );
-        }
-    }
-
-    public void modifyObject(Object object) throws FactException
-    {
-        if ( this.retractedObjects != null )
-        {
-            super.modifyObject( object );
-        }
-        else
-        {
-            this.assertedObjects.add( object );
-        }
-    }
-
-    public void retractObject(Object object) throws RetractionException
-    {
-        if ( this.retractedObjects != null )
-        {
-            super.retractObject( object );
-            this.retractedObjects.add( object );
-        }
-        else
-        {
-            this.assertedObjects.remove( object );
-        }
-    }
+    // ------------------------------------------------------------
+    //     Instance methods
+    // ------------------------------------------------------------
 
     /** Abort this <code>TransactionalWorkingMemory</code>,
      *  by tossing out all asserted objects, and reseting
      *  this <code>TransactionalWorkingMemory</code> to a
      *  clean state.
      */
-    public void abort()
+    public synchronized void abort()
     {
         this.assertedObjects.clear();
     }
@@ -84,8 +104,10 @@ public class TransactionalWorkingMemory extends WorkingMemory
     /** Commit all asserted objects into the logic engine,
      *  and reset this <code>TransactionalWorkingMemory</code>
      *  to a clean state.
+     *
+     *  @throws DroolsException If an 
      */
-    public void commit() throws DroolsException
+    public synchronized void commit() throws DroolsException
     {
         try
         {
@@ -112,6 +134,65 @@ public class TransactionalWorkingMemory extends WorkingMemory
         {
             this.assertedObjects.clear();
             this.retractedObjects = null;
+        }
+    }
+
+    /** Assert a new fact object into this working memory.
+     *
+     *  @param object The object to assert.
+     *
+     *  @throws AssertionException if an error occurs during assertion.
+     */
+    public synchronized void assertObject(Object object) throws AssertionException
+    {
+        if ( this.retractedObjects != null )
+        {
+            super.assertObject( object );
+        }
+        else
+        {
+            this.assertedObjects.add( object );
+        }
+    }
+
+    /** Modify a fact object in this working memory.
+     *
+     *  With the exception of time-based nodes, modification of
+     *  a fact object is semantically equivelent to retracting and
+     *  re-asserting it.
+     *
+     *  @param object The object to modify.
+     *
+     *  @throws FactException if an error occurs during modification.
+     */
+    public synchronized void modifyObject(Object object) throws FactException
+    {
+        if ( this.retractedObjects != null )
+        {
+            super.modifyObject( object );
+        }
+        else
+        {
+            this.assertedObjects.add( object );
+        }
+    }
+
+    /** Retract a fact object from this working memory.
+     *
+     *  @param object The object to retract.
+     *
+     *  @throws RetractionException if an error occurs during retraction.
+     */
+    public synchronized void retractObject(Object object) throws RetractionException
+    {
+        if ( this.retractedObjects != null )
+        {
+            super.retractObject( object );
+            this.retractedObjects.add( object );
+        }
+        else
+        {
+            this.assertedObjects.remove( object );
         }
     }
 }
