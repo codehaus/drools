a49d1c386db480c7ebbc39e85a825878879fe2cc
bob <bob@e9a468ab-b372-0410-861a-d954c37f4f16>
2002-08-18 17:32:05 +0000

    Adding some tests.

--- trunk/drools/drools-core/src/java/test/org/drools/io/SemanticsLoaderTest.java
+++ trunk/drools/drools-core/src/java/test/org/drools/io/SemanticsLoaderTest.java
@@ -4,6 +4,8 @@ import org.drools.smf.SemanticModule;
 
 import junit.framework.TestCase;
 
+import java.util.Set;
+
 public class SemanticsLoaderTest extends TestCase
 {
     SemanticsLoader loader;
@@ -41,5 +43,65 @@ public class SemanticsLoaderTest extends TestCase
         SemanticModule module = this.loader.load( "org.drools.semantics.java" );
 
         assertNotNull( module );
+
+        Set names = null;
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+        //     ObjectType
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+        names = module.getObjectTypeNames();
+
+        assertEquals( 1,
+                      names.size() );
+
+        assertTrue( names.contains( "class" ) );
+
+        assertEquals( org.drools.semantics.java.ClassObjectType.class,
+                      module.getObjectType( "class" ) );
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+        //     Condition
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+        names = module.getConditionNames();
+
+        assertEquals( 1,
+                      names.size() );
+
+        assertTrue( names.contains( "condition" ) );
+
+        assertEquals( org.drools.semantics.java.ExprCondition.class,
+                      module.getCondition( "condition" ) );
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+        //     FactExtractor
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+        names = module.getFactExtractorNames();
+
+        assertEquals( 1,
+                      names.size() );
+
+        assertTrue( names.contains( "extractor" ) );
+
+        assertEquals( org.drools.semantics.java.ExprExtractor.class,
+                      module.getFactExtractor( "extractor" ) );
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+        //     Consequence
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+        names = module.getConsequenceNames();
+
+        assertEquals( 1,
+                      names.size() );
+
+        System.err.println( "conseq names: " + names );
+
+        assertTrue( names.contains( "consequence" ) );
+
+        assertEquals( org.drools.semantics.java.BlockConsequence.class,
+                      module.getConsequence( "consequence" ) );   
     }
 }
