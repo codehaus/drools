a54a90e0b0fe5ecabc19c53b1739a5b9919c3eb9
bob <bob@e9a468ab-b372-0410-861a-d954c37f4f16>
2002-08-26 17:59:22 +0000

    Fixing a possible multi-threading issue with the java semantic module. Added jython dependency in prep for jython semantic module.

--- trunk/drools/drools-core/src/test/org/drools/semantics/java/InterpTest.java
+++ trunk/drools/drools-core/src/test/org/drools/semantics/java/InterpTest.java
@@ -4,6 +4,7 @@ import org.drools.rule.Declaration;
 import org.drools.spi.MockTuple;
 
 import bsh.EvalError;
+import bsh.NameSpace;
 
 import junit.framework.TestCase;
 
@@ -30,22 +31,26 @@ public class InterpTest extends TestCase
     {
         this.interp.setText( "42 + 12" );
 
+        NameSpace ns = new NameSpace( "" );
+
         assertEquals( new Integer(54),
-                      this.interp.evaluate() );
+                      this.interp.evaluate( ns ) );
     }
 
     public void testEvaluate_Variables() throws Exception
     {
+        NameSpace ns = new NameSpace( "" );
+
         this.interp.setText( "a + b" );
-
-        this.interp.setVariable( "a",
-                                 new Integer(42) );
-
-        this.interp.setVariable( "b",
-                                 new Integer(12) );
-
+        
+        ns.setVariable( "a",
+                        new Integer(42) );
+        
+        ns.setVariable( "b",
+                        new Integer(12) );
+        
         assertEquals( new Integer(54),
-                      this.interp.evaluate() );
+                      this.interp.evaluate( ns ) );
     }
 
     public void testEvaluate_Tuple() throws Exception
