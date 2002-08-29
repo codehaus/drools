4da6552e3ba98ddd01a621cedc796eb28ce86d28
bob <bob@e9a468ab-b372-0410-861a-d954c37f4f16>
2002-08-29 04:29:17 +0000

    Removed dependency upon jdring.

--- drools/drools-core/src/main/org/drools/reteoo/impl/Scheduler.java
+++ drools/drools-core/src/main/org/drools/reteoo/impl/Scheduler.java
@@ -1,62 +1,59 @@
 package org.drools.reteoo.impl;
 
 /*
- $Id: Scheduler.java,v 1.3 2002-08-22 07:42:39 bob Exp $
+  $Id: Scheduler.java,v 1.4 2002-08-29 04:29:17 bob Exp $
 
- Copyright 2002 (C) The Werken Company. All Rights Reserved.
+  Copyright 2002 (C) The Werken Company. All Rights Reserved.
  
- Redistribution and use of this software and associated documentation
- ("Software"), with or without modification, are permitted provided
- that the following conditions are met:
+  Redistribution and use of this software and associated documentation
+  ("Software"), with or without modification, are permitted provided
+  that the following conditions are met:
 
- 1. Redistributions of source code must retain copyright
-    statements and notices.  Redistributions must also contain a
-    copy of this document.
+  1. Redistributions of source code must retain copyright
+  statements and notices.  Redistributions must also contain a
+  copy of this document.
  
- 2. Redistributions in binary form must reproduce the
-    above copyright notice, this list of conditions and the
-    following disclaimer in the documentation and/or other
-    materials provided with the distribution.
+  2. Redistributions in binary form must reproduce the
+  above copyright notice, this list of conditions and the
+  following disclaimer in the documentation and/or other
+  materials provided with the distribution.
  
- 3. The name "drools" must not be used to endorse or promote
-    products derived from this Software without prior written
-    permission of The Werken Company.  For written permission,
-    please contact bob@werken.com.
+  3. The name "drools" must not be used to endorse or promote
+  products derived from this Software without prior written
+  permission of The Werken Company.  For written permission,
+  please contact bob@werken.com.
  
- 4. Products derived from this Software may not be called "drools"
-    nor may "drools" appear in their names without prior written
-    permission of The Werken Company. "drools" is a registered
-    trademark of The Werken Company.
+  4. Products derived from this Software may not be called "drools"
+  nor may "drools" appear in their names without prior written
+  permission of The Werken Company. "drools" is a registered
+  trademark of The Werken Company.
  
- 5. Due credit should be given to The Werken Company.
-    (http://drools.werken.com/).
+  5. Due credit should be given to The Werken Company.
+  (http://drools.werken.com/).
  
- THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS
- ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
- NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
- THE WERKEN COMPANY OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- OF THE POSSIBILITY OF SUCH DAMAGE.
+  THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS
+  ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
+  NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+  THE WERKEN COMPANY OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+  OF THE POSSIBILITY OF SUCH DAMAGE.
  
- */
+*/
 
 import org.drools.WorkingMemory;
 import org.drools.spi.ConsequenceException;
 
-import fr.dyade.jdring.AlarmManager;
-import fr.dyade.jdring.AlarmListener;
-import fr.dyade.jdring.AlarmEntry;
-import fr.dyade.jdring.PastDateException;
-
 import java.util.Date;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /** Scheduler for rules requiring truth duration.
  *
@@ -89,10 +86,10 @@ class Scheduler
     // ------------------------------------------------------------
 
     /** Alarm manager. */
-    private AlarmManager scheduler;
+    private Timer scheduler;
 
-    /** Scheduled entries. */
-    private Map entries;
+    /** Scheduled tasks. */
+    private Map tasks;
 
     // ------------------------------------------------------------
     //     Constructors
@@ -102,10 +99,9 @@ class Scheduler
      */
     private Scheduler()
     {
-        this.scheduler = new AlarmManager( true,
-                                           "DroolsScheduler" + new Date().getTime() );
+        this.scheduler = new Timer( true );
 
-        this.entries = new HashMap();
+        this.tasks = new HashMap();
     }
 
     /** Schedule an agenda item.
@@ -122,14 +118,16 @@ class Scheduler
 
         try
         {
-            AlarmEntry entry = this.scheduler.addAlarm( then,
-                                                        new AgendaItemFireListener( item,
-                                                                                    workingMemory ) );
+            TimerTask task = new AgendaItemFireListener( item,
+                                                         workingMemory );
 
-            this.entries.put( item,
-                              entry );
+            this.scheduler.schedule( task,
+                                     then );
+
+            this.tasks.put( item,
+                            task );
         }
-        catch (PastDateException e)
+        catch (Exception e)
         {
             e.printStackTrace();
         }
@@ -141,11 +139,11 @@ class Scheduler
      */
     void cancelAgendaItem(AgendaItem item)
     {
-        AlarmEntry entry = (AlarmEntry) this.entries.get( item );
+        TimerTask task = (TimerTask) this.tasks.get( item );
 
-        if ( entry != null )
+        if ( task != null )
         {
-            this.scheduler.removeAlarm( entry );
+            task.cancel();
         }
     }
 }
@@ -154,7 +152,7 @@ class Scheduler
  *
  *  @author <a href="mailto:bob@eng.werken.com">bob mcwhirter</a>
  */
-class AgendaItemFireListener implements AlarmListener
+class AgendaItemFireListener extends TimerTask
 {
     // ------------------------------------------------------------
     //     Instance members
@@ -191,10 +189,8 @@ class AgendaItemFireListener implements AlarmListener
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
     /** Handle the firing of an alarm.
-     *
-     *  @param alarmEntry The alarm entry.
      */
-    public void handleAlarm(AlarmEntry alarmEntry)
+    public void run()
     {
         try
         {
