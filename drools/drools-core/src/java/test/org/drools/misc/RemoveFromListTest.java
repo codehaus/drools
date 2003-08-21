bfd88bbcb158671489c84f9cff16ff8c9b5b5faf
tdiesler <tdiesler@e9a468ab-b372-0410-861a-d954c37f4f16>
2003-08-21 01:09:21 +0000

    improved test, still can't reproduce it

--- drools/drools-core/src/java/test/org/drools/misc/RemoveFromListTest.java
+++ drools/drools-core/src/java/test/org/drools/misc/RemoveFromListTest.java
@@ -8,64 +8,58 @@ import org.drools.io.RuleSetLoader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.io.FileReader;
 
 /**
  * A consequence removes an object from a list
  *
  * Autor: thomas.diesler@iotronics.com
  *
- * $Id: RemoveFromListTest.java,v 1.1 2003-08-20 23:50:37 tdiesler Exp $
+ * $Id: RemoveFromListTest.java,v 1.2 2003-08-21 01:09:21 tdiesler Exp $
  */
-public class RemoveFromListTest extends TestCase {
+public class RemoveFromListTest extends TestCase
+{
 
-   private WorkingMemory workingMemory;
+    private WorkingMemory workingMemory;
 
-   public RemoveFromListTest (String name) {
-      super(name);
-   }
+    public RemoveFromListTest( String name )
+    {
+        super( name );
+    }
 
-   public void setUp () throws Exception {
+    public void setUp() throws Exception
+    {
 
-      RuleBase ruleBase = new RuleBase();
+        RuleBase ruleBase = new RuleBase();
 
-      RuleSetLoader loader = new RuleSetLoader();
-      URL url = getClass().getResource("RemoveFromListTest.drl");
-      assertNotNull("cannot find drl file", url);
-      loader.load(url, ruleBase);
+        RuleSetLoader loader = new RuleSetLoader();
+        URL url = getClass().getResource( "RemoveFromListTest.drl" );
+        assertNotNull( "cannot find drl file", url );
+        //loader.load(url, ruleBase);
 
-      workingMemory = ruleBase.createWorkingMemory();
-   }
+        // for debuging
+        FileReader reader = new FileReader( "src/java/test/org/drools/misc/RemoveFromListTest.drl" );
+        loader.load( reader, ruleBase );
+        reader.close();
 
-   public void testRemoveFromList () throws Exception {
+        workingMemory = ruleBase.createWorkingMemory();
+    }
 
-      ArrayList list = new ArrayList();
-      list.add(new Participant(false));
-      list.add(new Participant(true));
+    public void testRemoveFromList() throws Exception
+    {
 
-      Iterator it = list.iterator();
-      while (it.hasNext()) {
-         Participant pt = (Participant) it.next();
-         workingMemory.assertObject(pt);
-      }
-   }
+        Participant pt1 = new Participant( false );
+        Participant pt2 = new Participant( true );
 
-   /**
-    * Participant
-    */
-   public static class Participant {
+        ArrayList list = new ArrayList();
+        list.add( pt1 );
+        list.add( pt2 );
 
-      private boolean active;
+        workingMemory.assertObject( list );
+        workingMemory.assertObject( pt1 );
+        workingMemory.assertObject( pt2 );
 
-      public Participant (boolean active) {
-         this.active = active;
-      }
-
-      public boolean isActive () {
-         return active;
-      }
-
-      public void setActive (boolean active) {
-         this.active = active;
-      }
-   }
+        assertEquals( "invalid number of participants", 1, list.size() );
+        assertEquals( "particpant should be inactive", false, ( (Participant) list.get( 0 ) ).isActive() );
+    }
 }
--- drools/drools-core/src/test/org/drools/misc/Participant.java
+++ drools/drools-core/src/test/org/drools/misc/Participant.java
@@ -1,27 +1,35 @@
 package org.drools.misc;
 
 
-
 /**
  * A consequence removes an object from a list
  *
  * Autor: thomas.diesler@iotronics.com
  *
- * $Id: Participant.java,v 1.1 2003-08-20 23:50:37 tdiesler Exp $
+ * $Id: Participant.java,v 1.2 2003-08-21 01:09:21 tdiesler Exp $
  */
-public class Participant {
+public class Participant
+{
 
-   private boolean active;
+    private boolean active;
 
-   public Participant (boolean active) {
-      this.active = active;
-   }
+    public Participant( boolean active )
+    {
+        this.active = active;
+    }
 
-   public boolean isActive () {
-      return active;
-   }
+    public boolean isActive()
+    {
+        return active;
+    }
 
-   public void setActive (boolean active) {
-      this.active = active;
-   }
+    public void setActive( boolean active )
+    {
+        this.active = active;
+    }
+
+    public String toString()
+    {
+        return "[Participant: active=" + active + "]";
+    }
 }
