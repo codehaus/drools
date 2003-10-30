1300da908ed57db99134ca14308b7fc707fd0c78
bob <bob@e9a468ab-b372-0410-861a-d954c37f4f16>
2003-10-30 15:08:59 +0000

    Removing the ability to build rulebases from bare Rules. You have to use RuleSets now.  All part of the move towards runtime rule-management and such.

--- trunk/drools/drools-core/src/main/org/drools/RuleBaseBuilder.java
+++ trunk/drools/drools-core/src/main/org/drools/RuleBaseBuilder.java
@@ -13,21 +13,10 @@ public class RuleBaseBuilder
         this.builder = new Builder();
     }
 
-    public void addRule(Rule rule)
-        throws RuleIntegrationException
-    {
-        this.builder.addRule( rule );
-    }
-
     public void addRuleSet(RuleSet ruleSet)
         throws RuleIntegrationException
     {
-        Rule[] rules = ruleSet.getRules();
-
-        for ( int i = 0 ; i < rules.length ; ++i )
-        {
-            this.builder.addRule( rules[ i ] );
-        }
+        this.builder.addRuleSet( ruleSet );
     }
 
     public RuleBase build()
