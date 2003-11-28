2a81da55ff396388961ada5ca24b68ce793b9000
bob <bob@e9a468ab-b372-0410-861a-d954c37f4f16>
2003-11-28 21:16:31 +0000

    Adding more factories.

---
+++ trunk/drools/drools-java/src/main/org/drools/semantics/java/BlockConsequenceFactory.java
@@ -0,0 +1,23 @@
+package org.drools.semantics.java;
+
+import org.drools.spi.Consequence;
+import org.drools.smf.ConsequenceFactory;
+import org.drools.smf.Configuration;
+import org.drools.smf.FactoryException;
+
+public class BlockConsequenceFactory
+    implements ConsequenceFactory
+{
+    private static final BlockConsequenceFactory INSTANCE = new BlockConsequenceFactory();
+
+    public static BlockConsequenceFactory getInstance()
+    {
+        return INSTANCE;
+    }
+
+    public Consequence newConsequence(Configuration config)
+        throws FactoryException
+    {
+        return new BlockConsequence( config.getText() );
+    }
+}
