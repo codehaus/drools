d3c6cea2389ffc41436d82a8341d11502bb23700
bob <bob@e9a468ab-b372-0410-861a-d954c37f4f16>
2003-11-28 07:29:03 +0000

    Adjusting for decls in the factories that need them.

--- trunk/drools/drools-core/src/main/org/drools/smf/ConditionFactory.java
+++ trunk/drools/drools-core/src/main/org/drools/smf/ConditionFactory.java
@@ -1,9 +1,11 @@
 package org.drools.smf;
 
+import org.drools.rule.Declaration;
 import org.drools.spi.Condition;
 
 public interface ConditionFactory
 {
-    Condition newCondition(Configuration config)
+    Condition newCondition(Declaration[] availDecls,
+                           Configuration config)
         throws FactoryException;
 }
