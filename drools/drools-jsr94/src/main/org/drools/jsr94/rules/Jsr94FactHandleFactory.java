e3edb38d85fa6abd81926525c90f53b9c9bfb648
bob <bob@e9a468ab-b372-0410-861a-d954c37f4f16>
2003-12-04 03:34:29 +0000

    Reworking the fact-handle stuff slightly.
    Updated the jsr94 jar to the final draft.

---
+++ drools/drools-jsr94/src/main/org/drools/jsr94/rules/Jsr94FactHandleFactory.java
@@ -0,0 +1,20 @@
+package org.drools.jsr94.rules;
+
+import org.drools.FactHandle;
+import org.drools.reteoo.FactHandleFactory;
+
+public class Jsr94FactHandleFactory
+    implements FactHandleFactory
+{
+    private long counter;
+
+    public Jsr94FactHandleFactory()
+    {
+        this.counter = 0;
+    }
+
+    public FactHandle newFactHandle()
+    {
+        return new Jsr94FactHandle( ++this.counter );
+    }
+}
