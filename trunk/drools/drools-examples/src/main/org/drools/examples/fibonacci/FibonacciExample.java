df3ca8c509a6869ef4fe09265706b7bef14b9596
bob <bob@e9a468ab-b372-0410-861a-d954c37f4f16>
2003-12-05 04:28:13 +0000

    Ah, adding fibonacci.

---
+++ trunk/drools/drools-examples/src/main/org/drools/examples/fibonacci/FibonacciExample.java
@@ -0,0 +1,35 @@
+package org.drools.examples.fibonacci;
+
+import org.drools.RuleBaseBuilder;
+import org.drools.RuleBase;
+import org.drools.WorkingMemory;
+import org.drools.io.RuleSetReader;
+import org.drools.rule.RuleSet;
+
+public class FibonacciExample
+{
+    public static void main(String[] args)
+        throws Exception
+    {
+        RuleSetReader reader = new RuleSetReader();
+        
+        RuleSet ruleSet = reader.read( FibonacciExample.class.getResource( "fibonacci.drl" ) );
+        
+        RuleBaseBuilder builder = new RuleBaseBuilder();
+        
+        builder.addRuleSet( ruleSet );
+        
+        RuleBase ruleBase = builder.build();
+        
+        WorkingMemory workingMemory = ruleBase.newWorkingMemory();
+        
+        Fibonacci fibonacci = new Fibonacci( 25 );
+
+        
+        workingMemory.assertObject( fibonacci );
+
+        workingMemory.fireAllRules();
+        
+        System.err.println( fibonacci.getValue() );
+    }
+}
