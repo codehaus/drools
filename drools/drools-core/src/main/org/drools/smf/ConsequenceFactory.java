a1c3c4a7b84fcf861908b40bfb7ae96c666901a7
bob <bob@e9a468ab-b372-0410-861a-d954c37f4f16>
2003-12-30 04:55:39 +0000

    Adding in Declaration[] to more factories, to detect errors early in the parse, possibly.

--- drools/drools-core/src/main/org/drools/smf/ConsequenceFactory.java
+++ drools/drools-core/src/main/org/drools/smf/ConsequenceFactory.java
@@ -1,9 +1,11 @@
 package org.drools.smf;
 
+import org.drools.rule.Declaration;
 import org.drools.spi.Consequence;
 
 public interface ConsequenceFactory
 {
-    Consequence newConsequence(Configuration config)
+    Consequence newConsequence(Configuration config,
+                               Declaration[] availDecls)
         throws FactoryException;
 }
--- drools/drools-core/src/main/org/drools/smf/DurationFactory.java
+++ drools/drools-core/src/main/org/drools/smf/DurationFactory.java
@@ -1,9 +1,11 @@
 package org.drools.smf;
 
+import org.drools.rule.Declaration;
 import org.drools.spi.Duration;
 
 public interface DurationFactory
 {
-    Duration newDuration(Configuration config)
+    Duration newDuration(Configuration config,
+                         Declaration[] availDecls)
         throws FactoryException;
 }
