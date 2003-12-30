0cc6c2baae2d20457d019747de2ee5695758b0bb
bob <bob@e9a468ab-b372-0410-861a-d954c37f4f16>
2003-12-30 21:54:15 +0000

    Adjusting imports so that jsr-94 builds

--- trunk/drools/drools-jsr94/src/test/org/drools/jsr94/rules/admin/LocalRuleExecutionSetProviderTestCase.java
+++ trunk/drools/drools-jsr94/src/test/org/drools/jsr94/rules/admin/LocalRuleExecutionSetProviderTestCase.java
@@ -1,7 +1,7 @@
 package org.drools.jsr94.rules.admin;
 
 /*
- $Id: LocalRuleExecutionSetProviderTestCase.java,v 1.3 2003-10-26 22:06:50 bob Exp $
+ $Id: LocalRuleExecutionSetProviderTestCase.java,v 1.4 2003-12-30 21:54:15 bob Exp $
 
  Copyright 2002 (C) The Werken Company. All Rights Reserved.
 
@@ -49,7 +49,7 @@ package org.drools.jsr94.rules.admin;
 import org.drools.rule.RuleSet;
 import org.drools.jsr94.rules.JSR94TestBase;
 import org.drools.io.RuleSetReader;
-import org.drools.io.SemanticsReader;
+import org.drools.smf.SemanticsReader;
 import org.drools.smf.SimpleSemanticsRepository;
 //import org.drools.io.RuleSetLoader;
 
