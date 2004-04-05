2a85f62c0a344f19fae5d686fa2ade6e6a3a4c42
n_alex <n_alex@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-04-05 20:19:50 +0000

    Beefed up the test class a bit.  It'll now make certain that unsupported AST representations passed into the createRuleExecutionSet(Serializable, Map) method will be recieved with an IllegalArgumentException.  Also, I make sure the InputStream is properly closed.  There are a lot of places in the tests and in the Impl code where input streams aren't properly closed, and I'm going to try to hunt them down and weed them out.  Thanks to Dain Sundstrom for pointing that out to me.

--- drools/drools-jsr94/src/test/org/drools/jsr94/rules/admin/RuleExecutionSetProviderTestCase.java
+++ drools/drools-jsr94/src/test/org/drools/jsr94/rules/admin/RuleExecutionSetProviderTestCase.java
@@ -4,13 +4,15 @@ import org.drools.jsr94.rules.RuleEngineTestBase;
 import org.drools.RuleBase;
 import org.drools.io.RuleSetReader;
 import org.drools.RuleBaseBuilder;
+import org.drools.rule.RuleSet;
 
 import javax.rules.admin.RuleAdministrator;
 import javax.rules.admin.RuleExecutionSet;
 import javax.rules.admin.RuleExecutionSetProvider;
-import java.io.Reader;
-import java.io.InputStreamReader;
-import java.io.InputStream;
+import javax.rules.admin.RuleExecutionSetCreateException;
+import java.io.*;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -22,9 +24,9 @@ import junit.framework.TestSuite;
  */
 public class RuleExecutionSetProviderTestCase extends RuleEngineTestBase
 {
-
     private RuleAdministrator ruleAdministrator;
     private RuleExecutionSetProvider ruleSetProvider;
+    private RuleBase ruleBase = null;
 
     /**
      * Setup the test case.
@@ -34,6 +36,50 @@ public class RuleExecutionSetProviderTestCase extends RuleEngineTestBase
         super.setUp();
         ruleAdministrator = ruleServiceProvider.getRuleAdministrator();
         ruleSetProvider = ruleAdministrator.getRuleExecutionSetProvider( null );
+
+        initRuleBase();
+    }
+
+    private void initRuleBase()
+    {
+        InputStream resourceAsStream = null;
+        try {
+            resourceAsStream = RuleEngineTestBase.class.getResourceAsStream(bindUri);
+            Reader reader = new InputStreamReader( resourceAsStream );
+            RuleSetReader ruleSetReader = new RuleSetReader();
+            RuleBaseBuilder builder = new RuleBaseBuilder();
+            builder.addRuleSet( ruleSetReader.read( reader ) );
+            ruleBase = builder.build();
+        }
+        catch (IOException e)
+        {
+            throw new ExceptionInInitializerError("setUp() could not init the " +
+                   "RuleBase due to an IOException in the InputStream: " + e);
+        }
+        catch (Exception e)
+        {
+           throw new ExceptionInInitializerError("setUp() could not init the " +
+                   "RuleBase, " + e);
+        }
+        finally
+        {
+            if (resourceAsStream != null)
+            {
+                try
+                {
+                    resourceAsStream.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    protected void tearDown()
+    {
+        ruleBase = null;
     }
 
     /**
@@ -47,19 +93,11 @@ public class RuleExecutionSetProviderTestCase extends RuleEngineTestBase
                 "been implemented.");
     }
 
-
     /**
      * Test createRuleExecutionSet from Serializable.
      */
     public void testCreateFromSerializable() throws Exception
     {
-        InputStream resourceAsStream = RuleEngineTestBase.class.getResourceAsStream(bindUri);
-        Reader reader = new InputStreamReader( resourceAsStream );
-        RuleSetReader ruleSetReader = new RuleSetReader();
-        RuleBaseBuilder builder = new org.drools.RuleBaseBuilder();
-        builder.addRuleSet( ruleSetReader.read( reader ) );
-        RuleBase ruleBase = builder.build();
-
         RuleExecutionSet ruleSet = ruleSetProvider.createRuleExecutionSet(ruleBase, null);
         assertEquals("rule set name", "Sisters Rules", ruleSet.getName());
         assertEquals("number of rules", 2, ruleSet.getRules().size());
@@ -74,6 +112,23 @@ public class RuleExecutionSetProviderTestCase extends RuleEngineTestBase
         RuleExecutionSet ruleSet = ruleSetProvider.createRuleExecutionSet(rulesUri, null);
         assertEquals("rule set name", "Sisters Rules", ruleSet.getName());
         assertEquals("number of rules", 2, ruleSet.getRules().size());
+    }
+
+    public void testIncompatibleSerializableCreation() throws Exception
+    {
+        try
+        {
+            RuleExecutionSet ruleSet = ruleSetProvider.createRuleExecutionSet(
+                    new ArrayList(), null);
+            fail("Should have thrown an IllegalArgumentException. ArrayList " +
+                    "objects are not valid AST representations.");
+        }
+        catch (IllegalArgumentException e)
+        {
+            /* this is supposed to happen if you pass in a serializable object
+             * that isn't a supported AST representation.
+             */
+        }
     }
 
     public static void main (String[] args) {
