7e304933fe3257a31809b6d410e30e23b4dc655a
mproctor <mproctor@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-06-05 11:50:16 +0000

    Updated MockWorkingMemory to implement addEventListener, removeEventListener, getListeners Updated MockTuple with getters and setters for WorkingMemory and Rule

--- drools/drools-groovy/src/test/org/drools/MockWorkingMemory.java
+++ drools/drools-groovy/src/test/org/drools/MockWorkingMemory.java
@@ -1,11 +1,39 @@
 package org.drools;
 
+import org.drools.WorkingMemory;
+import org.drools.event.WorkingMemoryEventListener;
+
 import java.util.List;
+
 
 public class MockWorkingMemory
     implements WorkingMemory
 {
     private Object appData;
+
+    /**
+     * add event listener to listeners ArrayList
+     * @param listener
+     */
+    public void addEventListener(WorkingMemoryEventListener listener) {
+        //do nothing
+    }    
+
+    /**
+     * remove event listener from listeners ArrayList
+     * @param listener
+     */
+    public void removeEventListener(WorkingMemoryEventListener listener) {
+      //do nothing
+    }
+
+    /**
+     * Returns a read-only list of listeners
+     * @return listeners
+     */
+    public List getListeners() {
+       return null;
+    }      
 
     public Object getApplicationData()
     {
--- drools/drools-groovy/src/test/org/drools/spi/MockTuple.java
+++ drools/drools-groovy/src/test/org/drools/spi/MockTuple.java
@@ -2,6 +2,8 @@ package org.drools.spi;
 
 import org.drools.FactHandle;
 import org.drools.rule.Declaration;
+import org.drools.WorkingMemory;
+import org.drools.rule.Rule;
 
 import java.util.Set;
 import java.util.Map;
@@ -9,6 +11,10 @@ import java.util.HashMap;
 
 public class MockTuple implements Tuple
 {
+
+    private Rule rule;    
+    private WorkingMemory workingMemory;
+    
     private Map tuple;
 
     public MockTuple()
@@ -37,4 +43,24 @@ public class MockTuple implements Tuple
     {
         return null;
     }
+
+    public void setRule(Rule rule)
+    {
+        this.rule = rule;
+    }
+
+    public Rule getRule()
+    {
+        return this.rule;
+    }
+
+    public void setWorkingMemory(WorkingMemory workingMemory)
+    {
+        this.workingMemory = workingMemory;
+    }
+
+    public WorkingMemory getWorkingMemory()
+    {
+        return this.workingMemory;
+    }      
 }
