eb9629244c0731233a9e35fbf109280e99c8e282
mproctor <mproctor@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-06-20 17:38:38 +0000

    removed debug statement

--- drools/drools-java/src/main/org/drools/semantics/java/BlockConsequence.java
+++ drools/drools-java/src/main/org/drools/semantics/java/BlockConsequence.java
@@ -1,7 +1,7 @@
 package org.drools.semantics.java;
 
 /*
- $Id: BlockConsequence.java,v 1.16 2004-06-15 18:24:17 bob Exp $
+ $Id: BlockConsequence.java,v 1.17 2004-06-20 17:38:38 mproctor Exp $
 
  Copyright 2002 (C) The Werken Company. All Rights Reserved.
 
@@ -59,7 +59,7 @@ import java.util.Iterator;
  *
  *  @author <a href="mailto:bob@werken.com">bob@werken.com</a>
  *
- *  @version $Id: BlockConsequence.java,v 1.16 2004-06-15 18:24:17 bob Exp $
+ *  @version $Id: BlockConsequence.java,v 1.17 2004-06-20 17:38:38 mproctor Exp $
  */
 public class BlockConsequence
     extends Interp
@@ -99,7 +99,6 @@ public class BlockConsequence
                        WorkingMemory workingMemory)
         throws ConsequenceException
     {
-        System.err.println( "firing for " + tuple );
         try
         {
             /*
