50aa27aa33559931c45ba55ed5ac93e1d4d1f72d
mproctor <mproctor@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-06-23 21:36:43 +0000

    Moved fibonacci example to be a sub directory of org/drools/examples/java

---
+++ trunk/drools/drools-examples/src/main/org/drools/examples/java/fibonacci/FibonacciExample.java
@@ -0,0 +1,40 @@
+package org.drools.examples.java.fibonacci;
+
+import org.drools.RuleBase;
+import org.drools.WorkingMemory;
+import org.drools.io.RuleSetReader;
+import org.drools.io.RuleBaseBuilder;
+import org.drools.rule.RuleSet;
+
+import org.drools.event.DefaultWorkingMemoryEventListener;
+import org.drools.event.WorkingMemoryEventListener;
+import org.drools.event.ObjectAssertedEvent;
+import org.drools.event.ObjectModifiedEvent;
+import org.drools.event.ObjectRetractedEvent;
+import org.drools.event.ConditionTestedEvent;
+import org.drools.event.ActivationCreatedEvent;
+import org.drools.event.ActivationFiredEvent;
+
+public class FibonacciExample
+{
+    public static void main(String[] args)
+        throws Exception
+    {
+        RuleBase ruleBase = RuleBaseBuilder.buildFromUrl( FibonacciExample.class.getResource( "fibonacci.drl" ) );
+
+        WorkingMemory workingMemory = ruleBase.newWorkingMemory();
+
+        Fibonacci fibonacci = new Fibonacci( 50 );
+
+        long start = System.currentTimeMillis();
+
+        workingMemory.assertObject( fibonacci );
+
+
+        workingMemory.fireAllRules();
+
+        long stop = System.currentTimeMillis();
+
+        System.err.println( "fibanacci(" + fibonacci.getSequence() + ") == " + fibonacci.getValue() + " took " + (stop-start) + "ms" );
+    }
+}
