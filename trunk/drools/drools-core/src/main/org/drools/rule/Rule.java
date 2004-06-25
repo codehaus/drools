b10406df87ffccba010c885b115c0fcacdeb21cd
mproctor <mproctor@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-06-25 01:57:26 +0000

    Rules now record the load order

--- trunk/drools/drools-core/src/main/org/drools/rule/Rule.java
+++ trunk/drools/drools-core/src/main/org/drools/rule/Rule.java
@@ -1,10 +1,10 @@
 package org.drools.rule;
 
 /*
- $Id: Rule.java,v 1.25 2003-11-21 04:18:13 bob Exp $
+ $Id: Rule.java,v 1.26 2004-06-25 01:57:26 mproctor Exp $
 
  Copyright 2001-2003 (C) The Werken Company. All Rights Reserved.
- 
+
  Redistribution and use of this software and associated documentation
  ("Software"), with or without modification, are permitted provided
  that the following conditions are met:
@@ -12,25 +12,25 @@ package org.drools.rule;
  1. Redistributions of source code must retain copyright
     statements and notices.  Redistributions must also contain a
     copy of this document.
- 
+
  2. Redistributions in binary form must reproduce the
     above copyright notice, this list of conditions and the
     following disclaimer in the documentation and/or other
     materials provided with the distribution.
- 
+
  3. The name "drools" must not be used to endorse or promote
     products derived from this Software without prior written
     permission of The Werken Company.  For written permission,
     please contact bob@werken.com.
- 
+
  4. Products derived from this Software may not be called "drools"
     nor may "drools" appear in their names without prior written
-    permission of The Werken Company. "drools" is a trademark of 
+    permission of The Werken Company. "drools" is a trademark of
     The Werken Company.
- 
+
  5. Due credit should be given to The Werken Company.
     (http://werken.com/)
- 
+
  THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS
  ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
  NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
@@ -43,7 +43,7 @@ package org.drools.rule;
  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  */
 
 import org.drools.spi.Condition;
@@ -68,7 +68,7 @@ import java.util.Collections;
  *
  *  @author <a href="mailto:bob@eng.werken.com">bob mcwhirter</a>
  *
- *  @version $Id: Rule.java,v 1.25 2003-11-21 04:18:13 bob Exp $
+ *  @version $Id: Rule.java,v 1.26 2004-06-25 01:57:26 mproctor Exp $
  */
 public class Rule
 {
@@ -109,6 +109,9 @@ public class Rule
 
     /** Truthness duration. */
     private Duration duration;
+
+    /** Load order in RuleSet*/
+    private long loadOrder;
 
     // ------------------------------------------------------------
     //     Constructors
@@ -203,7 +206,7 @@ public class Rule
      *  <p>
      *  Possibly exceptions include:
      *  </p>
-     * 
+     *
      *  <pre>
      *      NoParameterDeclarationException
      *  </pre>
@@ -264,13 +267,13 @@ public class Rule
      *
      *  @param declaration The <i>root fact object</i> <code>Declaration</code> to add.
      */
-    public void addParameterDeclaration(Declaration declaration) 
+    public void addParameterDeclaration(Declaration declaration)
     {
         if ( this.parameterDeclarations == Collections.EMPTY_SET )
         {
             this.parameterDeclarations = new HashSet();
         }
-            
+
         this.parameterDeclarations.add( declaration );
         addDeclaration( declaration );
     }
@@ -443,5 +446,15 @@ public class Rule
     public Consequence getConsequence()
     {
         return this.consequence;
+    }
+
+    public long getLoadOrder()
+    {
+      return loadOrder;
+    }
+
+    void setLoadOrder(long loadOrder)
+    {
+      this.loadOrder = loadOrder;
     }
 }
