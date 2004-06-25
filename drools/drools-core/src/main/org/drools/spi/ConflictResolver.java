9343d805cf6d433595fe0ea5420ed04c56ea485a
mproctor <mproctor@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-06-25 02:02:23 +0000

    ConflictResolvers now return a list of unresolved conflicts

--- drools/drools-core/src/main/org/drools/spi/ConflictResolver.java
+++ drools/drools-core/src/main/org/drools/spi/ConflictResolver.java
@@ -1,10 +1,10 @@
 package org.drools.spi;
 
 /*
- $Id: ConflictResolver.java,v 1.2 2004-06-20 16:07:10 n_alex Exp $
+ $Id: ConflictResolver.java,v 1.3 2004-06-25 02:02:23 mproctor Exp $
 
  Copyright 2001-2003 (C) The Werken Company. All Rights Reserved.
- 
+
  Redistribution and use of this software and associated documentation
  ("Software"), with or without modification, are permitted provided
  that the following conditions are met:
@@ -12,25 +12,25 @@ package org.drools.spi;
  1. Redistributions of source code must retain copyright
     statements and notices.  Redistributions must also contain a
     copy of this document.
- 
+
  2. Redistributions in binary form must reproduce the
     above copyright notice, this list of conditions and the
     following disclaimer in the documentation and/or other
     materials provided with the distribution.
- 
+
  3. The name "drools" must not be used to endorse or promote
     products derived from this Software without prior written
     permission of The Werken Company.  For written permission,
     please contact bob@werken.com.
- 
+
  4. Products derived from this Software may not be called "drools"
     nor may "drools" appear in their names without prior written
-    permission of The Werken Company. "drools" is a trademark of 
+    permission of The Werken Company. "drools" is a trademark of
     The Werken Company.
- 
+
  5. Due credit should be given to The Werken Company.
     (http://werken.com/)
- 
+
  THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS
  ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
  NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
@@ -43,7 +43,7 @@ package org.drools.spi;
  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  */
 
 import java.util.List;
@@ -62,10 +62,10 @@ import java.util.List;
  *
  *  @author <a href="mailto:bob@werken.com">bob mcwhirter</a>
  *
- *  @version $Id: ConflictResolver.java,v 1.2 2004-06-20 16:07:10 n_alex Exp $
+ *  @version $Id: ConflictResolver.java,v 1.3 2004-06-25 02:02:23 mproctor Exp $
  */
 public interface ConflictResolver
 {
-    void insert(Activation activation,
+    List insert(Activation activation,
                 List list);
 }
