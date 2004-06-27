e2dab21bf52b6b6abd5dc7b3aa7976d9cd851d4d
mproctor <mproctor@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-06-27 13:28:00 +0000

    Added state example from drools-59

---
+++ trunk/drools/drools-examples/src/main/org/drools/examples/java/state/State.java
@@ -0,0 +1,43 @@
+package org.drools.examples.java.state;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class State
+{
+    private String name;
+    private String state;
+
+    public State(String name)
+    {
+        this.name = name;
+        this.state = "NOTRUN";
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public String getState()
+    {
+        return this.state;
+    }
+
+    public String setState(String state)
+    {
+        return this.state = state;
+    }
+
+    public boolean inState(String name, String state)
+    {
+        boolean b = this.name.equals(name) && this.state.equals(state);
+        System.err.println("Testing " + this.name + "=" + name + "," + this.state + "=" + state + ":" + b);
+        return b;
+    }
+
+    public String toString()
+    {
+        return this.name + "[" + this.state + "]";
+    }
+}
