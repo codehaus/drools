c3cafe9845319c86916bdf8b1b0a5a37d30fbb99
dbarnett <dbarnett@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-07-10 00:06:37 +0000

    Updates to support XML-indented blocks of Python syntax

--- trunk/drools/drools-python/src/main/org/drools/semantics/python/Interp.java
+++ trunk/drools/drools-python/src/main/org/drools/semantics/python/Interp.java
@@ -1,7 +1,7 @@
 package org.drools.semantics.python;
 
 /*
- $Id: Interp.java,v 1.7 2004-06-30 21:46:33 bob Exp $
+ $Id: Interp.java,v 1.8 2004-07-10 00:06:37 dbarnett Exp $
 
  Copyright 2002 (C) The Werken Company. All Rights Reserved.
  
@@ -46,20 +46,28 @@ package org.drools.semantics.python;
  
  */
 
-import org.drools.WorkingMemory;
-import org.drools.rule.Declaration;
-import org.drools.spi.ObjectType;
-import org.drools.spi.Tuple;
-import org.drools.spi.KnowledgeHelper;
-
-import org.python.core.*;
-import org.python.parser.ast.modType;
-import org.python.util.PythonInterpreter;
-
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Hashtable;
 import java.util.Iterator;
-import java.util.Set;
 import java.util.Map;
+import java.util.Set;
+
+import org.drools.WorkingMemory;
+import org.drools.rule.Declaration;
+import org.drools.spi.KnowledgeHelper;
+import org.drools.spi.ObjectType;
+import org.drools.spi.Tuple;
+
+import org.python.core.Py;
+import org.python.core.PyCode;
+import org.python.core.PyDictionary;
+import org.python.core.PyString;
+import org.python.core.parser;
+import org.python.parser.ast.modType;
+import org.python.util.PythonInterpreter;
 
 /** Base class for Jython interpreter-based Python semantic components.
  *
@@ -68,10 +76,14 @@ import java.util.Map;
  *
  *  @author <a href="mailto:bob@eng.werken.com">bob mcwhirter</a>
  *
- *  @version $Id: Interp.java,v 1.7 2004-06-30 21:46:33 bob Exp $
+ *  @version $Id: Interp.java,v 1.8 2004-07-10 00:06:37 dbarnett Exp $
  */
 public class Interp
 {
+    /** The line separator system property ("\n" on UNIX). */
+    private static final String LINE_SEPARATOR =
+        System.getProperty("line.separator");
+    
     // ------------------------------------------------------------
     //     Class Initialization
     // ------------------------------------------------------------
@@ -107,11 +119,124 @@ public class Interp
     protected Interp(String text,
                      String type)
     {
-        this.text = text;
-        this.node = (modType) parser.parse( text,
+        this.text = stripOuterIndention(text);
+        this.node = (modType) parser.parse( this.text,
                                             type );
         this.code = Py.compile( this.node,
                                 "<jython>");
+    }
+    
+    /**
+     * Trims leading indention from the block of text. Since Python relies on
+     * indention as part of its syntax, any XML indention introduced needs to be
+     * stripped out.  For example, this:
+     * <pre>
+     * |   &lt;python:consequence&gt;
+     * |       if hello == 'Hello':
+     * |           print "Hi"
+     * |       else:
+     * |           print "Bye"
+     * |   &lt;/python:consequence&gt;
+     * </pre>
+     * is transformed into:
+     * <pre>
+     * |   &lt;python:consequence&gt;
+     * |if hello == 'Hello':
+     * |    print "Hi"
+     * |else:
+     * |    print "Bye"
+     * |   &lt;/python:consequence&gt;
+     * </pre>
+     * 
+     * @param text the block of text to be stripped
+     * @return the block of text stripped of its leading indention
+     */
+    private static String stripOuterIndention(String text)
+    {
+        try
+        {
+            if (null == text)
+            {
+                return null;
+            }
+            
+            BufferedReader br = new BufferedReader(
+                new InputStreamReader(
+                    new ByteArrayInputStream(text.getBytes())));
+            
+            StringBuffer unindentedText = new StringBuffer(text.length());
+            
+            int lineNo = 0;
+            try
+            {
+                String indent = null;
+                for (String line = br.readLine(); null != line; line = br.readLine())
+                {
+                    lineNo++;
+                    if ("".equals(line.trim()))
+                    {
+                        // Blank lines are passed through unmodified
+                        unindentedText.append(line + LINE_SEPARATOR);
+                        continue;
+                    }
+                    
+                    if (null == indent)
+                    {
+                        // The first non-bank line determines
+                        //   the outer indention level
+                        indent = line.substring(0, line.indexOf(line.trim()));
+                    }
+                    
+                    if (   (line.length() < indent.length())
+                        || (!line.matches("^" + indent + ".*")) )
+                    {
+                        // This can catch some poorly indented Python syntax
+                        throw new RuntimeException(
+                            "Bad Text Indention: Line " + lineNo + ": |" +
+                            formatForException(line) + "|" + LINE_SEPARATOR +
+                            formatForException(text));
+                    }
+                    
+                    // Remove the outer most indention from the line
+                    unindentedText.append(
+                        line.replaceFirst("^" + indent, "") + LINE_SEPARATOR);
+                }
+            } 
+            catch (IOException e)
+            {
+                throw new RuntimeException(e);
+            }
+            
+            // Remove extraneous trailing LINE_SEPARATOR
+            unindentedText.deleteCharAt(unindentedText.length() - 1);
+            
+            return unindentedText.toString();
+        }
+        catch (Exception e)
+        {
+            // [TODO]
+            // The whole point of this try/catch block is to ensure that exceptions
+            // make it out to the user; it seems something is swallowing everything
+            // except RuntimeExceptions.
+            if (e instanceof RuntimeException)
+            {
+                throw (RuntimeException) e;
+            }
+            
+            throw new RuntimeException(e);
+        }
+    }
+    
+    /**
+     * Helper method to format the text block for display in error messages.
+     * Since Python syntax errors can easily occur due to bad indention,
+     * this method replaces all tabs with "{{tab}}" and all spaces with ".".
+     * 
+     * @param text the text to be formatted
+     * @return the text with all tabs and spaces replaced for easier viewing
+     */
+    private static String formatForException(String text) {
+        return text.replaceAll("\t", "{{tab}}").replace(' ', '.');
     }
 
     // ------------------------------------------------------------
