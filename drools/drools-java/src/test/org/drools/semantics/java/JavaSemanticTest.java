bd3943e75fea437f945fc06c46bfa48b1e80491a
mproctor <mproctor@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-07-20 21:10:47 +0000

    Added java unit test

---
+++ drools/drools-java/src/test/org/drools/semantics/java/JavaSemanticTest.java
@@ -0,0 +1,28 @@
+/*
+ * Created on Jul 17, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.drools.semantics.java;
+
+import org.drools.smf.SMFTestFrameWork;
+
+/**
+ * @author mproctor
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class JavaSemanticTest extends SMFTestFrameWork
+{
+    public JavaSemanticTest( String name )
+    {
+          super( name );
+    }
+
+    public void setUp() throws Exception
+    {
+        super.setUp("java");        
+    }    
+}
