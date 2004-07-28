127c003f2df9a8ab71e5aff08441ba4d7d62514d
doug <doug@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-07-28 16:33:50 +0000

    added smf tests for groovy

---
+++ drools/drools-groovy/src/test/org/drools/semantics/groovy/GroovySemanticTest.java
@@ -0,0 +1,22 @@
+package org.drools.semantics.groovy;
+
+import org.drools.smf.SMFTestFrameWork;
+
+/**
+ * Extends SMFTestFrameWork specifying the groovy Semantic Module.
+ * The SMFTestFramework base class then loads the groovy test data files,
+ * extracts the tests for condition, extractors and consequences and
+ * executes them
+ */
+public class GroovySemanticTest extends SMFTestFrameWork
+{
+    public GroovySemanticTest( String name )
+    {
+          super( name );
+    }
+
+    public void setUp() throws Exception
+    {
+        super.setUp("groovy");
+    }
+}
