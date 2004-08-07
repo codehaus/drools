6c5178030c47ba884974fb65c7575ac8ee85d02c
mproctor <mproctor@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-08-07 11:16:17 +0000

    move serialisation implementatoin to drools base classes

--- drools/drools-jsr94/src/main/org/drools/jsr94/rules/Jsr94FactHandle.java
+++ drools/drools-jsr94/src/main/org/drools/jsr94/rules/Jsr94FactHandle.java
@@ -2,13 +2,11 @@ package org.drools.jsr94.rules;
 
 import org.drools.reteoo.FactHandleImpl;
 
-import java.io.Serializable;
-
 import javax.rules.Handle;
 
 public class Jsr94FactHandle
     extends FactHandleImpl
-    implements Handle, Serializable
+    implements Handle
 
 {
     Jsr94FactHandle()
