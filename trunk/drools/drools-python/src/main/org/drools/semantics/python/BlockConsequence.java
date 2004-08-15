eda9d3b5f8f443194cb6b37c3e46442b67a52456
mproctor <mproctor@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-08-15 15:53:18 +0000

    app data is already setup by setUpDictionary

--- trunk/drools/drools-python/src/main/org/drools/semantics/python/BlockConsequence.java
+++ trunk/drools/drools-python/src/main/org/drools/semantics/python/BlockConsequence.java
@@ -1,10 +1,10 @@
 package org.drools.semantics.python;
 
 /*
- $Id: BlockConsequence.java,v 1.7 2004-07-31 17:09:32 doug Exp $
+ $Id: BlockConsequence.java,v 1.8 2004-08-15 15:53:18 mproctor Exp $
 
  Copyright 2002 (C) The Werken Company. All Rights Reserved.
- 
+
  Redistribution and use of this software and associated documentation
  ("Software"), with or without modification, are permitted provided
  that the following conditions are met:
@@ -12,25 +12,25 @@ package org.drools.semantics.python;
  1. Redistributions of source code must retain copyright
     statements and notices.  Redistributions must also contain a
     copy of this document.
- 
+
  2. Redistributions in binary form must reproduce the
     above copyright notice, this list of conditions and the
     following disclaimer in the documentation and/or other
     materials provided with the distribution.
- 
+
  3. The name "drools" must not be used to endorse or promote
     products derived from this Software without prior written
     permission of The Werken Company.  For written permission,
     please contact bob@werken.com.
- 
+
  4. Products derived from this Software may not be called "drools"
     nor may "drools" appear in their names without prior written
     permission of The Werken Company. "drools" is a registered
     trademark of The Werken Company.
- 
+
  5. Due credit should be given to The Werken Company.
     (http://drools.werken.com/).
- 
+
  THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS
  ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
  NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
@@ -43,7 +43,7 @@ package org.drools.semantics.python;
  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  */
 
 import org.drools.WorkingMemory;
@@ -61,7 +61,7 @@ import java.util.Iterator;
  *
  *  @author <a href="mailto:bob@eng.werken.com">bob mcwhirter</a>
  *
- *  @version $Id: BlockConsequence.java,v 1.7 2004-07-31 17:09:32 doug Exp $
+ *  @version $Id: BlockConsequence.java,v 1.8 2004-08-15 15:53:18 mproctor Exp $
  */
 public class BlockConsequence
     extends Exec
@@ -84,9 +84,9 @@ public class BlockConsequence
     //     Instance methods
     // ------------------------------------------------------------
 
-    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     //     org.drools.spi.Consequence
-    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     /** Execute the consequence for the supplied
      *  matching <code>Tuple</code>.
@@ -101,18 +101,6 @@ public class BlockConsequence
                        WorkingMemory workingMemory) throws ConsequenceException
     {
         PyDictionary dict = setUpDictionary( tuple );
-
-        dict.setdefault( new PyString( "__drools_working_memory" ),
-                         Py.java2py( workingMemory ) );
-
-        Map appData = workingMemory.getApplicationDataMap();
-        for (Iterator iterator = appData.keySet().iterator(); iterator.hasNext();)
-        {
-            String key = (String) iterator.next();
-
-            dict.setdefault(new PyString(key),
-                            Py.java2py( appData.get(key) ) );
-        }
 
         try
         {
