de2312ab31f5b0e6dfd77948d7284e14e9f69c8a
mproctor <mproctor@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-08-15 16:09:38 +0000

    Added exception handling as setUpDictionary now needs to load classes if we have a nested class

--- drools/drools-python/src/main/org/drools/semantics/python/Eval.java
+++ drools/drools-python/src/main/org/drools/semantics/python/Eval.java
@@ -1,10 +1,10 @@
 package org.drools.semantics.python;
 
 /*
- $Id: Eval.java,v 1.8 2003-11-29 02:50:50 bob Exp $
+ $Id: Eval.java,v 1.9 2004-08-15 16:09:38 mproctor Exp $
 
  Copyright 2002 (C) The Werken Company. All Rights Reserved.
- 
+
  Redistribution and use of this software and associated documentation
  ("Software"), with or without modification, are permitted provided
  that the following conditions are met:
@@ -12,25 +12,25 @@ package org.drools.semantics.python;
  1. Redistributions of source code must retain copyright
     statements and notices.  Redistributions must also contain a
     copy of this document.
- 
+
  2. Redistributions in binary form must reproduce the
     above copyright notice, this list of conditions and the
     following disclaimer in the documentation and/or other
     materials provided with the distribution.
- 
+
  3. The name "drools" must not be used to endorse or promote
     products derived from this Software without prior written
     permission of The Werken Company.  For written permission,
     please contact bob@werken.com.
- 
+
  4. Products derived from this Software may not be called "drools"
     nor may "drools" appear in their names without prior written
     permission of The Werken Company. "drools" is a registered
     trademark of The Werken Company.
- 
+
  5. Due credit should be given to The Werken Company.
     (http://drools.werken.com/).
- 
+
  THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS
  ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
  NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
@@ -43,14 +43,21 @@ package org.drools.semantics.python;
  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  */
 
 import org.drools.rule.Declaration;
 import org.drools.spi.Tuple;
+import org.python.core.Py;
+import org.python.util.PythonInterpreter;
 import org.python.core.PyDictionary;
 import org.python.core.PyObject;
 import org.python.core.__builtin__;
+import org.python.core.PyFunction;
+import org.python.core.PyCode;
+import org.python.core.PyString;
+
+import org.drools.smf.SMFTestFrameWork;
 
 import java.util.Hashtable;
 
@@ -60,9 +67,9 @@ import java.util.Hashtable;
  *  @see ExprExtractor
  *
  *  @author <a href="mailto:bob@eng.werken.com">bob mcwhirter</a>
- *  @author <a href="mailto:christiaan@dacelo.nl">Christiaan ten Klooster</a>  
+ *  @author <a href="mailto:christiaan@dacelo.nl">Christiaan ten Klooster</a>
  *
- *  @version $Id: Eval.java,v 1.8 2003-11-29 02:50:50 bob Exp $
+ *  @version $Id: Eval.java,v 1.9 2004-08-15 16:09:38 mproctor Exp $
  */
 public class Eval
     extends Interp
@@ -74,6 +81,7 @@ public class Eval
     /** Required decls. */
     private Declaration[] decls;
 
+    //  private PyFunction evalFunc;
     // ------------------------------------------------------------
     //     Constructors
     // ------------------------------------------------------------
@@ -88,6 +96,19 @@ public class Eval
                "eval" );
 
         this.decls = analyze( availDecls );
+
+/*
+        String evalString = //"from org.drools.smf import SMFTestFrameWork\n" +
+                            //"from org.drools.smf import SMFTestFrameWork\n" +
+                            "def droolsFunction(code, globals, locals):\n" +
+                            "  return eval(code, globals, locals)\n";
+
+        Interp interp = new Interp(evalString, "exec");
+        PythonInterpreter pythonInterpreter = new PythonInterpreter();
+        pythonInterpreter.exec(interp.getCode());
+        evalFunc = (PyFunction) pythonInterpreter.get("droolsFunction", PyFunction.class);
+*/
+
     }
 
     // ------------------------------------------------------------
@@ -100,10 +121,10 @@ public class Eval
      *
      *  @return The result of evaluation.
      */
-    public Object evaluate(Tuple tuple) 
+    public Object evaluate(Tuple tuple) throws Exception
     {
         PyDictionary dict = setUpDictionary( tuple );
-        
+
         return evaluate( dict );
     }
 
@@ -113,23 +134,21 @@ public class Eval
      *
      *  @return The result of evaluation.
      */
-    protected Object evaluate(PyDictionary locals) 
+    protected Object evaluate(PyDictionary locals) throws Exception
     {
         PyDictionary globals = new PyDictionary( new Hashtable() );
 
-        PyObject result =  __builtin__.eval( getCode(),
-                                             locals,
-                                             globals );
+        PyObject result = __builtin__.eval(getCode(), globals, locals);
 
-        return result.__tojava__(Object.class); 
+        return result.__tojava__(Object.class);
     }
-    
-    
+
+
     /** Evaluate.
      *
      *  @return The result of evaluation.
      */
-    protected Object evaluate()
+    protected Object evaluate() throws Exception
     {
         PyDictionary locals = new PyDictionary( new Hashtable() );
 
@@ -151,7 +170,7 @@ public class Eval
         throws Exception
     {
         ExprAnalyzer analyzer = new ExprAnalyzer();
-            
+
         return analyzer.analyze( getNode(),
                                  availDecls );
     }
