7125dd68de57459b5ebc9daa3ea12fe0e4476754
mproctor <mproctor@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-10-06 13:52:26 +0000

    Interface changed to work with PriorityQueue system

--- drools/drools-core/src/main/org/drools/spi/ConflictResolver.java
+++ drools/drools-core/src/main/org/drools/spi/ConflictResolver.java
@@ -1,31 +1,31 @@
 package org.drools.spi;
 
 /*
- * $Id: ConflictResolver.java,v 1.6 2004-09-17 00:14:12 mproctor Exp $
- * 
+ * $Id: ConflictResolver.java,v 1.7 2004-10-06 13:52:26 mproctor Exp $
+ *
  * Copyright 2001-2003 (C) The Werken Company. All Rights Reserved.
- * 
+ *
  * Redistribution and use of this software and associated documentation
  * ("Software"), with or without modification, are permitted provided that the
  * following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain copyright statements and
  * notices. Redistributions must also contain a copy of this document.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
- * 
+ *
  * 3. The name "drools" must not be used to endorse or promote products derived
  * from this Software without prior written permission of The Werken Company.
  * For written permission, please contact bob@werken.com.
- * 
+ *
  * 4. Products derived from this Software may not be called "drools" nor may
  * "drools" appear in their names without prior written permission of The Werken
  * Company. "drools" is a trademark of The Werken Company.
- * 
+ *
  * 5. Due credit should be given to The Werken Company. (http://werken.com/)
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS ``AS IS''
  * AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -37,30 +37,31 @@ package org.drools.spi;
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *  
+ *
  */
 
 import java.io.Serializable;
-import java.util.List;
+import java.util.Comparator;
 
 /**
  * Strategy for resolving conflicts amongst multiple rules.
- * 
+ *
  * <p>
  * Since a fact or set of facts may activate multiple rules, a
  * <code>ConflictResolutionStrategy</code> is used to provide priority
  * ordering of conflicting rules.
  * </p>
- * 
+ *
  * @see Activation
  * @see Tuple
  * @see org.drools.rule.Rule
- * 
+ *
  * @author <a href="mailto:bob@werken.com">bob mcwhirter </a>
- * 
- * @version $Id: ConflictResolver.java,v 1.6 2004-09-17 00:14:12 mproctor Exp $
+ * @author <a href="mailto:simon@redhillconsulting.com.au">Simon Harris</a>
+ *
+ * @version $Id: ConflictResolver.java,v 1.7 2004-10-06 13:52:26 mproctor Exp $
  */
-public interface ConflictResolver extends Serializable
+public interface ConflictResolver extends Serializable, Comparator
 {
-    List insert(Activation activation, List list);
+    int compare( Activation existing, Activation adding );
 }
