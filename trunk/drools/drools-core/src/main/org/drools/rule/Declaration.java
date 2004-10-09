29f97c1dc7773e6c84e3a51941f5dde4ac277214
simon <simon@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-10-09 06:59:03 +0000

    Optimise equals/hashCode implementations

--- trunk/drools/drools-core/src/main/org/drools/rule/Declaration.java
+++ trunk/drools/drools-core/src/main/org/drools/rule/Declaration.java
@@ -1,31 +1,31 @@
 package org.drools.rule;
 
 /*
- * $Id: Declaration.java,v 1.18 2004-09-17 00:14:11 mproctor Exp $
- * 
+ * $Id: Declaration.java,v 1.19 2004-10-09 06:59:01 simon Exp $
+ *
  * Copyright 2001-2003 (C) The Werken Company. All Rights Reserved.
- * 
+ *
  * Redistribution and use of this software and associated documentation
  * ("Software"), with or without modification, are permitted provided that the
  * following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain copyright statements and
  * notices. Redistributions must also contain a copy of this document.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
- * 
+ *
  * 3. The name "drools" must not be used to endorse or promote products derived
  * from this Software without prior written permission of The Werken Company.
  * For written permission, please contact bob@werken.com.
- * 
+ *
  * 4. Products derived from this Software may not be called "drools" nor may
  * "drools" appear in their names without prior written permission of The Werken
  * Company. "drools" is a trademark of The Werken Company.
- * 
+ *
  * 5. Due credit should be given to The Werken Company. (http://werken.com/)
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS ``AS IS''
  * AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -37,19 +37,19 @@ package org.drools.rule;
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *  
+ *
  */
-
-import java.io.Serializable;
 
 import org.drools.spi.ObjectType;
 
+import java.io.Serializable;
+
 /**
  * A typed, named variable for <code>Condition</code> evaluation.
- * 
+ *
  * @see ObjectType
  * @see org.drools.spi.Condition
- * 
+ *
  * @author <a href="mailto:bob@eng.werken.com">bob mcwhirter </a>
  */
 public class Declaration implements Serializable
@@ -77,7 +77,7 @@ public class Declaration implements Serializable
 
     /**
      * Construct.
-     * 
+     *
      * @param objectType The type of this variable declaration.
      * @param identifier The name of the variable.
      */
@@ -89,7 +89,7 @@ public class Declaration implements Serializable
 
     /**
      * Construct.
-     * 
+     *
      * @param identifier The name of the variable.
      */
     public Declaration(String identifier)
@@ -103,7 +103,7 @@ public class Declaration implements Serializable
 
     /**
      * Set the <code>ObjectType</code>.
-     * 
+     *
      * @param objectType The object-type.
      */
     public void setObjectType(ObjectType objectType)
@@ -113,7 +113,7 @@ public class Declaration implements Serializable
 
     /**
      * Retrieve the <code>ObjectType</code>.
-     * 
+     *
      * @return The object-type.
      */
     public ObjectType getObjectType()
@@ -123,7 +123,7 @@ public class Declaration implements Serializable
 
     /**
      * Retrieve the variable's identifier.
-     * 
+     *
      * @return The variable's identifier.
      */
     public String getIdentifier()
@@ -138,12 +138,16 @@ public class Declaration implements Serializable
      */
     public boolean equals(Object thatObj)
     {
+        if (this == thatObj )
+        {
+            return true;
+        }
+
         if ( thatObj instanceof Declaration )
         {
             Declaration that = ( Declaration ) thatObj;
 
-            return ( this.objectType.equals( that.objectType ) && this.identifier
-                                                                                 .equals( that.identifier ) );
+            return this.objectType.equals( that.objectType ) && this.identifier.equals( that.identifier );
         }
 
         return false;
@@ -154,7 +158,7 @@ public class Declaration implements Serializable
      */
     public int hashCode()
     {
-        return ( this.objectType.hashCode( ) + this.identifier.hashCode( ) );
+        return this.objectType.hashCode( ) ^ this.identifier.hashCode( );
     }
 
     public String toString()
