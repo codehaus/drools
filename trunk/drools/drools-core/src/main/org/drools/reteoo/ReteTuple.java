7b70d11806322810291cd6655e91e5c4f86babd6
simon <simon@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-10-11 23:24:57 +0000

    Removed redundant fact handle lookup

--- trunk/drools/drools-core/src/main/org/drools/reteoo/ReteTuple.java
+++ trunk/drools/drools-core/src/main/org/drools/reteoo/ReteTuple.java
@@ -1,31 +1,31 @@
 package org.drools.reteoo;
 
 /*
- * $Id: ReteTuple.java,v 1.32 2004-09-17 00:59:56 mproctor Exp $
- * 
+ * $Id: ReteTuple.java,v 1.33 2004-10-11 23:24:57 simon Exp $
+ *
  * Copyright 2001-2003 (C) The Werken Company. All Rights Reserved.
- * 
+ *
  * Redistribution and use of this software and associated documentation
  * ("Software"), with or without modification, are permitted provided that the
  * following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain copyright statements and
  * notices. Redistributions must also contain a copy of this document.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
- * 
+ *
  * 3. The name "drools" must not be used to endorse or promote products derived
  * from this Software without prior written permission of The Werken Company.
  * For written permission, please contact bob@werken.com.
- * 
+ *
  * 4. Products derived from this Software may not be called "drools" nor may
  * "drools" appear in their names without prior written permission of The Werken
  * Company. "drools" is a trademark of The Werken Company.
- * 
+ *
  * 5. Due credit should be given to The Werken Company. (http://werken.com/)
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS ``AS IS''
  * AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -37,14 +37,8 @@ package org.drools.reteoo;
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *  
+ *
  */
-
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 
 import org.drools.FactHandle;
 import org.drools.WorkingMemory;
@@ -52,14 +46,20 @@ import org.drools.rule.Declaration;
 import org.drools.rule.Rule;
 import org.drools.spi.Tuple;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Base Rete-OO <code>Tuple</code> implementation.
- * 
+ *
  * @see Tuple
- * 
+ *
  * @author <a href="mailto:bob@werken.com">bob mcwhirter </a>
- * 
- * @version $Id: ReteTuple.java,v 1.32 2004-09-17 00:59:56 mproctor Exp $
+ *
+ * @version $Id: ReteTuple.java,v 1.33 2004-10-11 23:24:57 simon Exp $
  */
 class ReteTuple implements Tuple, Serializable
 {
@@ -108,7 +108,7 @@ class ReteTuple implements Tuple, Serializable
 
     /**
      * Copy constructor.
-     * 
+     *
      * @param that The tuple to copy.
      */
     ReteTuple(ReteTuple that)
@@ -126,7 +126,7 @@ class ReteTuple implements Tuple, Serializable
 
     /**
      * Construct a simple 1-column tuple.
-     * 
+     *
      * @param declaration The column declaration.
      * @param handle The fact-handle.
      * @param value The column value.
@@ -163,7 +163,7 @@ class ReteTuple implements Tuple, Serializable
 
     /**
      * Set a key column's value.
-     * 
+     *
      * @param declaration The column declaration.
      * @param handle The fact-handle.
      * @param value The value.
@@ -183,7 +183,7 @@ class ReteTuple implements Tuple, Serializable
 
     /**
      * Add all columns from another tuple.
-     * 
+     *
      * @param that The column source tuple.
      */
     public void putAll(ReteTuple that)
@@ -205,7 +205,7 @@ class ReteTuple implements Tuple, Serializable
 
     /**
      * Set an other column's value.
-     * 
+     *
      * @param declaration The column declaration.
      * @param value The value.
      */
@@ -216,7 +216,7 @@ class ReteTuple implements Tuple, Serializable
 
     /**
      * Retrieve the key for this tuple.
-     * 
+     *
      * @return The key.
      */
     TupleKey getKey()
@@ -226,7 +226,7 @@ class ReteTuple implements Tuple, Serializable
 
     /**
      * Retrieve the other columns for this tuple.
-     * 
+     *
      * @return The other columns.
      */
     Map getOtherColumns()
@@ -236,9 +236,9 @@ class ReteTuple implements Tuple, Serializable
 
     /**
      * Determine if this tuple depends upon a specified object.
-     * 
+     *
      * @param handle The object handle to test.
-     * 
+     *
      * @return <code>true</code> if this tuple depends upon the specified
      *         object, otherwise <code>false</code>.
      */
@@ -297,7 +297,6 @@ class ReteTuple implements Tuple, Serializable
             while ( it.hasNext( ) )
             {
                 Declaration decl = ( Declaration ) it.next( );
-                Object factObject = this.key.get( decl );
                 fact = ( FactHandleImpl ) this.key.get( decl );
                 if ( fact.getRecency( ) > currentRecency )
                 {
