176561a63ac7b5c0664aa9bdd6ed7a5dc647eba8
mproctor <mproctor@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-10-25 14:05:57 +0000

    remove print statements

--- drools/drools-core/src/test/org/drools/reteoo/RuleBaseTest.java
+++ drools/drools-core/src/test/org/drools/reteoo/RuleBaseTest.java
@@ -86,7 +86,6 @@ public class RuleBaseTest extends DroolsTestCase
         RuleBase ruleBase = builder.buildRuleBase( );
 
         // Serialize to a byte array
-        System.out.println( "Serialise out" );
         ByteArrayOutputStream bos = new ByteArrayOutputStream( );
         ObjectOutput out = new ObjectOutputStream( bos );
         out.writeObject( ruleBase );
@@ -96,7 +95,6 @@ public class RuleBaseTest extends DroolsTestCase
         byte[] bytes = bos.toByteArray( );
 
         // Deserialize from a byte array
-        System.out.println( "Serialise in" );
         ObjectInput in = new ObjectInputStream(
                                                 new ByteArrayInputStream( bytes ) );
         ruleBase = ( RuleBase ) in.readObject( );
