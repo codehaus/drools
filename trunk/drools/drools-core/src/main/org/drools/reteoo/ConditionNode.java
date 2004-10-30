b12260d1c5b23b1ad0b2309c4e50289051184278
simon <simon@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-10-30 12:43:28 +0000

    ReteTuple and TupleKey are now immutable

--- trunk/drools/drools-core/src/main/org/drools/reteoo/ConditionNode.java
+++ trunk/drools/drools-core/src/main/org/drools/reteoo/ConditionNode.java
@@ -1,31 +1,31 @@
 package org.drools.reteoo;
 
 /*
- * $Id: ConditionNode.java,v 1.16 2004-09-23 23:13:00 mproctor Exp $
- * 
+ * $Id: ConditionNode.java,v 1.17 2004-10-30 12:43:28 simon Exp $
+ *
  * Copyright 2001-2003 (C) The Werken Company. All Rights Reserved.
- * 
+ *
  * Redistribution and use of this software and associated documentation
  * ("Software"), with or without modification, are permitted provided that the
  * following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain copyright statements and
  * notices. Redistributions must also contain a copy of this document.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
- * 
+ *
  * 3. The name "drools" must not be used to endorse or promote products derived
  * from this Software without prior written permission of The Werken Company.
  * For written permission, please contact bob@werken.com.
- * 
+ *
  * 4. Products derived from this Software may not be called "drools" nor may
  * "drools" appear in their names without prior written permission of The Werken
  * Company. "drools" is a trademark of The Werken Company.
- * 
+ *
  * 5. Due credit should be given to The Werken Company. (http://werken.com/)
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS ``AS IS''
  * AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -37,12 +37,8 @@ package org.drools.reteoo;
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *  
+ *
  */
-
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
 
 import org.drools.AssertionException;
 import org.drools.FactException;
@@ -50,18 +46,22 @@ import org.drools.FactHandle;
 import org.drools.RetractionException;
 import org.drools.spi.Condition;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * Node which filters <code>ReteTuple</code>s.
- * 
+ *
  * <p>
  * Using a semantic <code>Condition</code>, this node may allow or disallow
  * <code>Tuples</code> to proceed further through the Rete-OO network.
  * </p>
- * 
+ *
  * @see ConditionNode
  * @see Condition
  * @see ReteTuple
- * 
+ *
  * @author <a href="mailto:bob@eng.werken.com">bob mcwhirter </a>
  */
 class ConditionNode extends TupleSource implements TupleSink
@@ -85,7 +85,7 @@ class ConditionNode extends TupleSource implements TupleSink
 
     /**
      * Construct.
-     * 
+     *
      * @param tupleSource The source of incoming <code>Tuples</code>.
      * @param condition The semantic <code>Condition</code>.
      */
@@ -107,7 +107,7 @@ class ConditionNode extends TupleSource implements TupleSink
 
     /**
      * Retrieve the <code>Condition</code> associated with this node.
-     * 
+     *
      * @return The <code>Condition</code>.
      */
     public Condition getCondition()
@@ -122,7 +122,7 @@ class ConditionNode extends TupleSource implements TupleSink
     /**
      * Retrieve the <code>Set</code> of <code>Declaration</code> s in the
      * propagated <code>Tuples</code>.
-     * 
+     *
      * @return The <code>Set</code> of <code>Declarations</code> in progated
      *         <code>Tuples</code>.
      */
@@ -137,29 +137,27 @@ class ConditionNode extends TupleSource implements TupleSink
 
     /**
      * Assert a new <code>Tuple</code>.
-     * 
+     *
      * @param tuple The <code>Tuple</code> being asserted.
      * @param workingMemory The working memory seesion.
      * 
      * @throws AssertionException If an error occurs while asserting.
      */
     public void assertTuple(ReteTuple tuple, WorkingMemoryImpl workingMemory) throws AssertionException
-    {        
+    {
         if ( getCondition( ).isAllowed( tuple ) )
         {
-            tuple
-                 .setConditionTimeStamp( order,
-                                         workingMemory.getConditionTimeStamp( ) );
+//            tuple.setConditionTimeStamp( order, workingMemory.getConditionTimeStamp() );
             propagateAssertTuple( tuple, workingMemory );
         }
     }
 
     /**
      * Retract tuples.
-     * 
+     *
      * @param key The tuple key.
      * @param workingMemory The working memory seesion.
-     * 
+     *
      * @throws RetractionException If an error occurs while retracting.
      */
     public void retractTuples(TupleKey key, WorkingMemoryImpl workingMemory) throws RetractionException
@@ -169,11 +167,11 @@ class ConditionNode extends TupleSource implements TupleSink
 
     /**
      * Modify tuples.
-     * 
+     *
      * @param trigger The root fact object handle.
      * @param newTuples Modification replacement tuples.
      * @param workingMemory The working memory session.
-     * 
+     *
      * @throws FactException If an error occurs while modifying.
      */
     public void modifyTuples(FactHandle trigger,
@@ -218,7 +216,7 @@ class ConditionNode extends TupleSource implements TupleSink
 
     /**
      * Produce a debug string.
-     * 
+     *
      * @return The debug string.
      */
     public String toString()
