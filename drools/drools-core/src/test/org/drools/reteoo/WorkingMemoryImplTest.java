7a14a484f82f192641030de48b81c219cbdfaf0b
mproctor <mproctor@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-11-07 12:57:59 +0000

    Added unit test for double serialization

--- drools/drools-core/src/test/org/drools/reteoo/WorkingMemoryImplTest.java
+++ drools/drools-core/src/test/org/drools/reteoo/WorkingMemoryImplTest.java
@@ -1,34 +1,62 @@
 /*
- * The RedHill Consulting, Pty. Ltd. Software License, Version 1.0
+ * $Id: WorkingMemoryImplTest.java,v 1.3 2004/09/16 23:43:08 mproctor
+ * Exp $
+ * Copyright 2001-2003 (C) The Werken Company. All Rights Reserved.
  *
- * Copyright (c) 2003-04 RedHill Consulting, Pty. Ltd.  All rights reserved.
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided that the
+ * following conditions are met:
  *
- * Redistribution and use in source or binary forms IS NOT PERMITTED
- * without prior written permission.
+ * 1. Redistributions of source code must retain copyright statements and
+ * notices. Redistributions must also contain a copy of this document.
  *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL REDHILL CONSULTING OR ITS CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
- * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
- * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. The name "drools" must not be used to endorse or promote products derived
+ * from this Software without prior written permission of The Werken Company.
+ * For written permission, please contact bob@werken.com.
+ *
+ * 4. Products derived from this Software may not be called "drools" nor may
+ * "drools" appear in their names without prior written permission of The Werken
+ * Company. "drools" is a trademark of The Werken Company.
+ *
+ * 5. Due credit should be given to The Werken Company. (http://werken.com/)
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE WERKEN COMPANY OR ITS CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
  */
 package org.drools.reteoo;
 
 import junit.framework.TestCase;
 import org.drools.FactHandle;
+import org.drools.WorkingMemory;
+import org.drools.RuleBase;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 import java.util.List;
 
 /**
  * @author <a href="mailto:simon@redhillconsulting.com.au">Simon Harris</a>
- * @version $Id: WorkingMemoryImplTest.java,v 1.1 2004-10-26 23:05:23 simon Exp $
+ * @version $Id: WorkingMemoryImplTest.java,v 1.2 2004-11-07 12:57:59 mproctor Exp $
  */
 public class WorkingMemoryImplTest extends TestCase
 {
@@ -36,20 +64,121 @@ public class WorkingMemoryImplTest extends TestCase
     {
         Object fact = new Object();
 
-        WorkingMemoryImpl impl = new WorkingMemoryImpl( new RuleBaseImpl( new Rete( ) ) );
+        WorkingMemory memory = new WorkingMemoryImpl( new RuleBaseImpl( new Rete( ) ) );
 
-        FactHandle handleA = impl.assertObject( fact );
+        FactHandle handleA = memory.assertObject( fact );
 
         assertNotNull( handleA );
 
-        FactHandle handleB = impl.assertObject( fact );
+        FactHandle handleB = memory.assertObject( fact );
 
         assertSame( handleA, handleB );
 
-        List objects = impl.getObjects( );
+        List objects = memory.getObjects( );
 
         assertNotNull( objects );
         assertEquals( 1, objects.size( ) );
         assertSame( fact, objects.get( 0 ) );
+    }
+
+    public void testSerializationSame() throws Exception
+    {
+        TestObject fact = new TestObject();
+
+        WorkingMemory memory = getWorkingMemory( new RuleBaseImpl( new Rete( ) ) );
+
+        //Serialise twice to test DROOLS-182
+        memory = serializeWorkingMemory( memory );
+        memory = serializeWorkingMemory( memory );
+
+        FactHandle handleA = memory.assertObject( fact );
+
+        assertNotNull( handleA );
+
+        FactHandle handleB = memory.assertObject( fact );
+
+        assertSame( handleA, handleB );
+
+        List objects = memory.getObjects( );
+
+        assertNotNull( objects );
+        assertEquals( 1, objects.size( ) );
+        assertSame( fact, objects.get( 0 ) );
+
+        //Serialise twice to test DROOLS-182
+        memory = serializeWorkingMemory( memory );
+        memory = serializeWorkingMemory( memory );
+    }
+
+    public void testSerializationNotSame() throws Exception
+    {
+        TestObject fact = new TestObject();
+
+        WorkingMemory memory = getWorkingMemory( new RuleBaseImpl( new Rete( ) ) );
+
+        FactHandle handleA = memory.assertObject( fact );
+
+        assertNotNull( handleA );
+
+        //Serialise twice to test DROOLS-182
+        memory = serializeWorkingMemory( memory );
+        memory = serializeWorkingMemory( memory );
+
+        FactHandle handleB = memory.assertObject( fact );
+
+        assertNotSame( handleA, handleB );
+        //assertEquals( handleA, handleB );
+
+        List objects = memory.getObjects( );
+
+        assertNotNull( objects );
+        assertEquals( 2, objects.size( ) );
+
+        //Serialise twice to test DROOLS-182
+        memory = serializeWorkingMemory( memory );
+        memory = serializeWorkingMemory( memory );
+    }
+
+    private static WorkingMemory getWorkingMemory(RuleBase ruleBase) throws Exception
+    {
+        // Serialize to a byte array
+        ByteArrayOutputStream bos = new ByteArrayOutputStream( );
+        ObjectOutput out = new ObjectOutputStream( bos );
+        out.writeObject( ruleBase.newWorkingMemory( ) );
+        out.close( );
+
+        // Get the bytes of the serialized object
+        byte[] bytes = bos.toByteArray( );
+
+        // Deserialize from a byte array
+        ObjectInput in = new ObjectInputStream(
+                                                new ByteArrayInputStream( bytes ) );
+        WorkingMemory workingMemoryOut = ( WorkingMemory ) in.readObject( );
+        in.close( );
+        return workingMemoryOut;
+    }
+
+    private static WorkingMemory serializeWorkingMemory(WorkingMemory workingMemoryIn) throws Exception
+    {
+        // Serialize to a byte array
+        ByteArrayOutputStream bos = new ByteArrayOutputStream( );
+        ObjectOutput out = new ObjectOutputStream( bos );
+        out.writeObject( workingMemoryIn );
+        out.close( );
+
+        // Get the bytes of the serialized object
+        byte[] bytes = bos.toByteArray( );
+
+        // Deserialize from a byte array
+        ObjectInput in = new ObjectInputStream(
+                                                new ByteArrayInputStream( bytes ) );
+        WorkingMemory workingMemoryOut = ( WorkingMemory ) in.readObject( );
+        in.close( );
+        return workingMemoryOut;
+    }
+
+    final public static class TestObject implements Serializable
+    {
+
     }
 }
