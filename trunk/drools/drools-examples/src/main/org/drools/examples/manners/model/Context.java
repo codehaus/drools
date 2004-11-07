66bcfac2bd8db810258a7abfff96970a6ba81cc3
mproctor <mproctor@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-11-07 18:41:45 +0000

    Added model

--- trunk/drools/drools-examples/src/main/org/drools/examples/manners/model/Context.java
+++ trunk/drools/drools-examples/src/main/org/drools/examples/manners/model/Context.java
@@ -1,32 +1,32 @@
-package org.drools.examples.manners.model;
+package org.drools.examples.model;
 
 /*
- * $Id: Context.java,v 1.2 2004-09-17 00:37:55 mproctor Exp $
- * 
+ * $Id: Context.java,v 1.3 2004-11-07 18:41:45 mproctor Exp $
+ *
  * Copyright 2002 (C) The Werken Company. All Rights Reserved.
- * 
+ *
  * Redistribution and use of this software and associated documentation
  * ("Software"), with or without modification, are permitted provided that the
  * following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain copyright statements and
  * notices. Redistributions must also contain a copy of this document.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
- * 
+ *
  * 3. The name "drools" must not be used to endorse or promote products derived
  * from this Software without prior written permission of The Werken Company.
  * For written permission, please contact bob@werken.com.
- * 
+ *
  * 4. Products derived from this Software may not be called "drools" nor may
  * "drools" appear in their names without prior written permission of The Werken
  * Company. "drools" is a registered trademark of The Werken Company.
- * 
+ *
  * 5. Due credit should be given to The Werken Company.
  * (http://drools.werken.com/).
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS ``AS IS''
  * AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -38,10 +38,12 @@ package org.drools.examples.manners.model;
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *  
+ *
  */
 
-public class Context
+import java.io.Serializable;
+
+public class Context  implements Serializable
 {
     private String state;
 
