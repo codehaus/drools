61b32487f86900bbb0abf10eeed6d8ea6de8a950
dbarnett <dbarnett@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-11-17 15:01:10 +0000

    added a couple tests to mimic something done in the JSR94 TCK that broke the PrimitiveLongMap

--- trunk/drools/drools-core/src/test/org/drools/reteoo/RuleBaseTest.java
+++ trunk/drools/drools-core/src/test/org/drools/reteoo/RuleBaseTest.java
@@ -1,12 +1,44 @@
 package org.drools.reteoo;
 
-import org.drools.DroolsTestCase;
-import org.drools.RuleBase;
-import org.drools.conflict.DefaultConflictResolver;
-import org.drools.rule.Rule;
-import org.drools.rule.RuleSet;
-import org.drools.spi.InstrumentedExtractor;
-import org.drools.spi.MockObjectType;
+/*
+ * $Id: RuleBaseTest.java,v 1.9 2004-11-17 15:01:10 dbarnett Exp $
+ *
+ * Copyright 2004 (C) The Werken Company. All Rights Reserved.
+ *
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain copyright statements and
+ * notices. Redistributions must also contain a copy of this document.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. The name "drools" must not be used to endorse or promote products derived
+ * from this Software without prior written permission of The Werken Company.
+ * For written permission, please contact bob@werken.com.
+ *
+ * 4. Products derived from this Software may not be called "drools" nor may
+ * "drools" appear in their names without prior written permission of The Werken
+ * Company. "drools" is a trademark of The Werken Company.
+ *
+ * 5. Due credit should be given to The Werken Company. (http://werken.com/)
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE WERKEN COMPANY OR ITS CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -14,6 +46,16 @@ import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
+import java.util.List;
+
+import org.drools.DroolsTestCase;
+import org.drools.RuleBase;
+import org.drools.WorkingMemory;
+import org.drools.conflict.DefaultConflictResolver;
+import org.drools.rule.Rule;
+import org.drools.rule.RuleSet;
+import org.drools.spi.InstrumentedExtractor;
+import org.drools.spi.MockObjectType;
 
 public class RuleBaseTest extends DroolsTestCase
 {
@@ -73,5 +115,17 @@ public class RuleBaseTest extends DroolsTestCase
         ObjectInput in = new ObjectInputStream( new ByteArrayInputStream( bytes ) );
         in.readObject( );
         in.close( );
+        
+        // JSR-94 Tests
+        // Reset the session. [ruleSession.reset();]
+        WorkingMemory newWorkingMemory = ruleBase.newWorkingMemory( );
+        assertNotNull("WMNotNullTest ", newWorkingMemory);
+        
+        // Retrieve all the objects, nothing should be here. The
+        // reset should have taken care of the removal.
+        // [ruleSession.getObjects();]
+        List objects = newWorkingMemory.getObjects( );
+        assertNotNull("ValuesNotNullTest ", objects);
+        assertEquals("ValuesZeroSizeTest ", 0, objects.size());
     }
 }
