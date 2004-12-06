346a4c9fe9aa68cb193dceebb57f8d7b77458454
dbarnett <dbarnett@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-12-06 00:45:30 +0000

    Cleanup of drools-base code to minimize CheckStyle errors (error count went from over 250 down to 31; mostly Javadoc errors remain)

--- drools/drools-base/src/main/org/drools/semantics/base/ClassFieldObjectTypeFactory.java
+++ drools/drools-base/src/main/org/drools/semantics/base/ClassFieldObjectTypeFactory.java
@@ -1,15 +1,55 @@
 package org.drools.semantics.base;
 
+/*
+ * $Id: ClassFieldObjectTypeFactory.java,v 1.5 2004-12-06 00:45:30 dbarnett Exp $
+ *
+ * Copyright 2004 (C) The Werken Company. All Rights Reserved.
+ *
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain copyright statements and
+ * notices. Redistributions must also contain a copy of this document.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. The name "drools" must not be used to endorse or promote products derived
+ * from this Software without prior written permission of The Werken Company.
+ * For written permission, please contact bob@werken.com.
+ *
+ * 4. Products derived from this Software may not be called "drools" nor may
+ * "drools" appear in their names without prior written permission of The Werken
+ * Company. "drools" is a registered trademark of The Werken Company.
+ *
+ * 5. Due credit should be given to The Werken Company.
+ * (http://drools.werken.com/).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE WERKEN COMPANY OR ITS CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 import org.drools.smf.Configuration;
 import org.drools.smf.FactoryException;
 import org.drools.smf.ObjectTypeFactory;
 import org.drools.spi.ImportEntry;
 import org.drools.spi.ObjectType;
-
-import java.lang.reflect.Method;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
 
 public class ClassFieldObjectTypeFactory implements ObjectTypeFactory
 {
@@ -20,23 +60,23 @@ public class ClassFieldObjectTypeFactory implements ObjectTypeFactory
         return INSTANCE;
     }
 
-    public ObjectType newObjectType(Configuration config, Set imports) throws FactoryException
+    public ObjectType newObjectType( Configuration config, Set imports ) throws FactoryException
     {
         String className = config.getText( );
-        String fieldName = config.getAttribute("field");
-        String fieldValue = config.getAttribute("value");
+        String fieldName = config.getAttribute( "field" );
+        String fieldValue = config.getAttribute( "value" );
 
-        if ( className == null || className.trim( ).equals( "" ))
+        if ( className == null || className.trim( ).equals( "" ) )
         {
             throw new FactoryException( "no class name specified" );
         }
 
-        if ( fieldName == null || fieldName.trim( ).equals( "" ))
+        if ( fieldName == null || fieldName.trim( ).equals( "" ) )
         {
             throw new FactoryException( "no field name specified" );
         }
 
-        if ( fieldValue == null || fieldValue.trim( ).equals( "" ))
+        if ( fieldValue == null || fieldValue.trim( ).equals( "" ) )
         {
             throw new FactoryException( "no field value specified" );
         }
@@ -44,16 +84,15 @@ public class ClassFieldObjectTypeFactory implements ObjectTypeFactory
         try
         {
             //get imports
-            Set importSet = new HashSet();
-            if (imports != null)
+            Set importSet = new HashSet( );
+            if ( imports != null )
             {
-
-                Iterator it = imports.iterator();
+                Iterator it = imports.iterator( );
                 ImportEntry importEntry;
-                while (it.hasNext())
+                while ( it.hasNext( ) )
                 {
-                    importEntry = (ImportEntry) it.next();
-                    importSet.add(importEntry.getImportEntry());
+                    importEntry = ( ImportEntry ) it.next( );
+                    importSet.add( importEntry.getImportEntry( ) );
                 }
             }
             ClassLoader cl = Thread.currentThread( ).getContextClassLoader( );
@@ -64,79 +103,85 @@ public class ClassFieldObjectTypeFactory implements ObjectTypeFactory
             {
                 clazz = cl.loadClass( className );
             }
-            catch ( Exception e )
+            catch ( ClassNotFoundException e )
             {
-                //swallow
-            }
-
-            /* Now try the className with each of the given imports */
-            if (clazz == null)
-            {
-                Iterator it = importSet.iterator();
+                /* Now try the className with each of the given imports */
+                Iterator it = importSet.iterator( );
                 while ( it.hasNext( ) && clazz == null )
                 {
-                    clazz = importClass(cl, (String) it.next(), className.trim( )) ;
+                    clazz = importClass( cl, ( String ) it.next( ), className.trim( ) ) ;
                 }
             }
+            
             /* We still can't find the class so throw an exception */
-            if (clazz == null)
+            if ( clazz == null )
             {
-                throw new FactoryException( "Unable to find class " + className);
+                throw new FactoryException( "Unable to find class " + className );
             }
 
             //make sure field getter exists
-            Method getterMethod = clazz.getMethod("get" + fieldName.toUpperCase( ).charAt( 0 ) + fieldName.substring( 1 ),
-                                                  null);
+            clazz.getMethod(
+                "get" + fieldName.toUpperCase( ).charAt( 0 ) + fieldName.substring( 1 ),
+                null );
 
-            return new ClassFieldObjectType( clazz, fieldName, fieldValue);
+            return new ClassFieldObjectType( clazz, fieldName, fieldValue );
         }
         catch ( SecurityException e )
         {
-            throw new FactoryException( "Field " + fieldName + " is not accessible for Class " + className);
+            throw new FactoryException( "Field " + fieldName + " is not accessible for Class " + className );
         }
         catch ( NoSuchMethodException e )
         {
-            throw new FactoryException( "Field " + fieldName + " does not exist for Class " + className);
+            throw new FactoryException( "Field " + fieldName + " does not exist for Class " + className );
         }
     }
 
-    private Class importClass(ClassLoader cl, String importText, String className)
+    private Class importClass( ClassLoader cl, String importText, String className )
     {
         String qualifiedClass = null;
         Class clazz = null;
-        if (importText.startsWith("from "))
+        
+        String convertedImportText;
+        if ( importText.startsWith( "from " ) )
         {
-            importText = converPythonImport(importText);
+            convertedImportText = converPythonImport( importText );
         }
+        else
+        {
+            convertedImportText = importText;
+        }
+        
         //not python
-        if (importText.endsWith("*"))
+        if ( convertedImportText.endsWith( "*" ) )
         {
-            qualifiedClass = importText.substring(0, importText.indexOf('*')) + className;
+            qualifiedClass = convertedImportText.substring( 0, convertedImportText.indexOf( '*' ) ) + className;
         }
-        else if (importText.endsWith(className))
+        else if ( convertedImportText.endsWith( className ) )
         {
-            qualifiedClass = importText;
+            qualifiedClass = convertedImportText;
         }
 
-        if (qualifiedClass != null)
+        if ( qualifiedClass != null )
         {
             try
             {
                 clazz = cl.loadClass( qualifiedClass );
             }
-            catch ( Exception e )
+            catch ( ClassNotFoundException e )
             {
-                //swallow
+                clazz = null;
             }
         }
         return clazz;
     }
 
-    private String converPythonImport(String packageText)
+    private String converPythonImport( String packageText )
     {
-        int fromIndex = packageText.indexOf("from ");
-        int importIndex = packageText.indexOf("import ");
-        return packageText.substring(fromIndex + 5, importIndex).trim() + "." +
-               packageText.substring(importIndex + 7).trim();
+        String fromString = "from ";
+        String importString = "import ";
+        int fromIndex = packageText.indexOf( fromString );
+        int importIndex = packageText.indexOf( importString );
+        return packageText.substring( fromIndex + fromString.length(), importIndex ).trim( ) + "."
+             + packageText.substring( importIndex + importString.length() ).trim( );
     }
 }
