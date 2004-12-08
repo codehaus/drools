da08c46e66dd8c354ad568a2ceba9b523aa41d4e
simon <simon@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-12-08 23:47:06 +0000

    DROOLS-219: Rename like named classes in different packages / semantic modules

--- trunk/drools/drools-core/src/test/org/drools/rule/RuleTest.java
+++ trunk/drools/drools-core/src/test/org/drools/rule/RuleTest.java
@@ -45,7 +45,7 @@ public class RuleTest extends DroolsTestCase
     {
         Rule rule = new Rule( "test-rule" );
 
-        Declaration paramDecl = rule.addParameterDeclaration( "paramVar", new MockObjectType() );
+        Declaration paramDecl = rule.addParameterDeclaration( "paramVar", new MockObjectType( true ) );
 
         List paramDecls = rule.getParameterDeclarations( );
 
@@ -62,7 +62,7 @@ public class RuleTest extends DroolsTestCase
     {
         Rule rule = new Rule( "test-rule" );
 
-        Declaration paramDecl = rule.addParameterDeclaration( "paramVar", new MockObjectType() );
+        Declaration paramDecl = rule.addParameterDeclaration( "paramVar", new MockObjectType( true ) );
 
         List paramDecls = rule.getParameterDeclarations( );
         assertLength( 1, paramDecls );
@@ -109,8 +109,8 @@ public class RuleTest extends DroolsTestCase
     {
         Rule rule = new Rule( "test-rule" );
 
-        assertEquals( 0, rule.addParameterDeclaration( "paramVar0", new MockObjectType() ).getIndex( ) );
-        assertEquals( 1, rule.addParameterDeclaration( "paramVar1", new MockObjectType() ).getIndex( ) );
+        assertEquals( 0, rule.addParameterDeclaration( "paramVar0", new MockObjectType( true ) ).getIndex( ) );
+        assertEquals( 1, rule.addParameterDeclaration( "paramVar1", new MockObjectType( true ) ).getIndex( ) );
     }
 
     public void testDuration_SimpleLong() throws Exception
@@ -163,7 +163,7 @@ public class RuleTest extends DroolsTestCase
 
         Rule rule = new Rule( "test-rule" );
 
-        rule.addParameterDeclaration( "paramVar", new MockObjectType() );
+        rule.addParameterDeclaration( "paramVar", new MockObjectType( true ) );
 
         //add consequence
         rule.setConsequence( new org.drools.spi.InstrumentedConsequence( ) );
