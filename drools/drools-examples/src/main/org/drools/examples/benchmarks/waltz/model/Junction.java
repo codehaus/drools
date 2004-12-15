d24e1e8068802853d57c37bfa15c39b4bc6745ee
dbarnett <dbarnett@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-12-15 15:13:42 +0000

    Implemented the Waltz Benchmark in Drools which follows the original OPS5 Benchmark as closely as possible. < http://jira.codehaus.org/browse/DROOLS-245 >

---
+++ drools/drools-examples/src/main/org/drools/examples/benchmarks/waltz/model/Junction.java
@@ -0,0 +1,98 @@
+package org.drools.examples.benchmarks.waltz.model;
+
+/*
+ * $Id: Junction.java,v 1.1 2004-12-15 15:13:42 dbarnett Exp $
+ *
+ * Copyright 2004 (C) The Werken Company. All Rights Reserved.
+ *
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain copyright statements and
+ * notices. Redistributions must also contain a copy of this document.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. The name "drools" must not be used to endorse or promote products derived
+ * from this Software without prior written permission of The Werken Company.
+ * For written permission, please contact bob@werken.com.
+ *
+ * 4. Products derived from this Software may not be called "drools" nor may
+ * "drools" appear in their names without prior written permission of The Werken
+ * Company. "drools" is a registered trademark of The Werken Company.
+ *
+ * 5. Due credit should be given to The Werken Company.
+ * (http://drools.werken.com/).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE WERKEN COMPANY OR ITS CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * Junctions are defined by 4 points. The basepoint is where the 3 (2) lines
+ * intersect. The points p1, p2, p3 are the other endpoints of the lines
+ * at this junction.
+ *
+ * (literalize junction p1 p2 p3 base_point type)
+ */
+public class Junction
+{
+    private int p1;
+    private int p2;
+    private int p3;
+    private int basePoint;
+    private String type;
+
+    public Junction( int p1, int p2, int p3, int basePoint, String type )
+    {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+        this.basePoint = basePoint;
+        this.type = type;
+    }
+
+    public int getP1( )
+    {
+        return this.p1;
+    }
+
+    public int getP2( )
+    {
+        return this.p2;
+    }
+
+    public int getP3( )
+    {
+        return this.p3;
+    }
+
+    public int getBasePoint( )
+    {
+        return this.basePoint;
+    }
+
+    public String getType( )
+    {
+        return this.type;
+    }
+
+    public String toString( )
+    {
+        return "Junction[p1=" + this.p1 + ", p2=" + this.p2 + ", p3=" + this.p3
+            + ", basePoint=" + this.basePoint + ", type=" + this.type + "]";
+    }
+}
