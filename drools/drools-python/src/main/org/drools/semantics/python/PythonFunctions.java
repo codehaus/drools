55c2e9f1e88a013ae3ac530659d077876cf9daca
dbarnett <dbarnett@e9a468ab-b372-0410-861a-d954c37f4f16>
2004-12-15 16:00:06 +0000

    Removed code copied from PythonInterp into PythonFunctions and made PythonFunctions call the method in PythonInterp. < http://jira.codehaus.org/browse/DROOLS-246 >

--- drools/drools-python/src/main/org/drools/semantics/python/PythonFunctions.java
+++ drools/drools-python/src/main/org/drools/semantics/python/PythonFunctions.java
@@ -1,32 +1,32 @@
 package org.drools.semantics.python;
 
 /*
- * $Id: PythonFunctions.java,v 1.1 2004-12-14 21:00:29 mproctor Exp $
- * 
- * Copyright 2002 (C) The Werken Company. All Rights Reserved.
- * 
+ * $Id: PythonFunctions.java,v 1.2 2004-12-15 16:00:06 dbarnett Exp $
+ *
+ * Copyright 2004 (C) The Werken Company. All Rights Reserved.
+ *
  * Redistribution and use of this software and associated documentation
  * ("Software"), with or without modification, are permitted provided that the
  * following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain copyright statements and
  * notices. Redistributions must also contain a copy of this document.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
- * 
+ *
  * 3. The name "drools" must not be used to endorse or promote products derived
  * from this Software without prior written permission of The Werken Company.
  * For written permission, please contact bob@werken.com.
- * 
+ *
  * 4. Products derived from this Software may not be called "drools" nor may
  * "drools" appear in their names without prior written permission of The Werken
  * Company. "drools" is a registered trademark of The Werken Company.
- * 
+ *
  * 5. Due credit should be given to The Werken Company.
  * (http://drools.werken.com/).
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS ``AS IS''
  * AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -38,29 +38,22 @@ package org.drools.semantics.python;
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *  
+ *
  */
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 import org.drools.spi.Functions;
 
 /**
- * Python block semantics <code>Consequence</code>.
+ * Python block semantics <code>Functions</code>.
  * 
  * @author <a href="mailto:bob@eng.werken.com">bob mcwhirter </a>
  * 
- * @version $Id: PythonFunctions.java,v 1.1 2004-12-14 21:00:29 mproctor Exp $
+ * @version $Id: PythonFunctions.java,v 1.2 2004-12-15 16:00:06 dbarnett Exp $
  */
 public class PythonFunctions implements Functions
 {
-   
     private String text;
 
-    /** The line separator system property ("\n" on UNIX). */
-    private static final String LINE_SEPARATOR = System.getProperty( "line.separator" );    
     // ------------------------------------------------------------
     //     Constructors
     // ------------------------------------------------------------
@@ -72,7 +65,7 @@ public class PythonFunctions implements Functions
      */
     public PythonFunctions(String name, String text)
     {
-        this.text = stripOuterIndention( text );
+        this.text = PythonInterp.stripOuterIndention( text );
     }
 
     public String getText()
@@ -84,135 +77,5 @@ public class PythonFunctions implements Functions
     {
         return "python";
     }
-    /**
-     * Trims leading indention from the block of text. Since Python relies on
-     * indention as part of its syntax, any XML indention introduced needs to be
-     * stripped out. For example, this:
-     * 
-     * <pre>
-     * 
-     *  |   &lt;python:consequence&gt;
-     *  |       if hello == 'Hello':
-     *  |           print &quot;Hi&quot;
-     *  |       else:
-     *  |           print &quot;Bye&quot;
-     *  |   &lt;/python:consequence&gt;
-     *  
-     * </pre>
-     * 
-     * is transformed into:
-     * 
-     * <pre>
-     * 
-     *  |   &lt;python:consequence&gt;
-     *  |if hello == 'Hello':
-     *  |    print &quot;Hi&quot;
-     *  |else:
-     *  |    print &quot;Bye&quot;
-     *  |   &lt;/python:consequence&gt;
-     *  
-     * </pre>
-     * 
-     * @param text the block of text to be stripped
-     * @return the block of text stripped of its leading indention
-     */
-    private static String stripOuterIndention(String text)
-    {
-        try
-        {
-            if ( null == text )
-            {
-                return null;
-            }
-
-            BufferedReader br = new BufferedReader(
-                                                    new InputStreamReader(
-                                                                           new ByteArrayInputStream(
-                                                                                                     text
-                                                                                                         .getBytes( ) ) ) );
-
-            StringBuffer unindentedText = new StringBuffer( text.length( ) );
-
-            int lineNo = 0;
-            try
-            {
-                String indent = null;
-                for ( String line = br.readLine( ); null != line; line = br
-                                                                           .readLine( ) )
-                {
-                    lineNo++;
-                    if ( "".equals( line.trim( ) ) )
-                    {
-                        // Blank lines are passed through unmodified
-                        unindentedText.append( line + LINE_SEPARATOR );
-                        continue;
-                    }
-
-                    if ( null == indent )
-                    {
-                        // The first non-bank line determines
-                        //   the outer indention level
-                        indent = line
-                                     .substring( 0, line.indexOf( line.trim( ) ) );
-                    }
-
-                    if ( ( line.length( ) < indent.length( ) )
-                         || ( !line.matches( "^" + indent + ".*" ) ) )
-                    {
-                        // This can catch some poorly indented Python syntax
-                        throw new RuntimeException( "Bad Text Indention: Line "
-                                                    + lineNo + ": |"
-                                                    + formatForException( line )
-                                                    + "|" + LINE_SEPARATOR
-                                                    + formatForException( text ) );
-                    }
-
-                    // Remove the outer most indention from the line
-                    unindentedText.append( line.replaceFirst( "^" + indent, "" )
-                                           + LINE_SEPARATOR );
-                }
-            }
-            catch ( IOException e )
-            {
-                throw new RuntimeException( e.getMessage( ) );
-            }
-
-            // Remove extraneous trailing LINE_SEPARATOR
-            if ( unindentedText.length( ) > 0 )
-            {
-                unindentedText.deleteCharAt( unindentedText.length( ) - 1 );
-            }
-
-            return unindentedText.toString( );
-        }
-        catch ( Exception e )
-        {
-            // [TODO]
-            // The whole point of this try/catch block is to ensure that
-            // exceptions
-            // make it out to the user; it seems something is swallowing
-            // everything
-            // except RuntimeExceptions.
-            if ( e instanceof RuntimeException )
-            {
-                throw ( RuntimeException ) e;
-            }
-
-            throw new RuntimeException( e.getMessage( ) );
-        }
-    }
-
-    /**
-     * Helper method to format the text block for display in error messages.
-     * Since Python syntax errors can easily occur due to bad indention, this
-     * method replaces all tabs with "{{tab}}" and all spaces with ".".
-     * 
-     * @param text the text to be formatted
-     * @return the text with all tabs and spaces replaced for easier viewing
-     */
-    private static String formatForException(String text)
-    {
-        return text.replaceAll( "\t", "{{tab}}" ).replace( ' ', '.' );
-    }    
 }
 
