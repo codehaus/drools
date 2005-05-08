825bf51624f2084ea7bc0ff5d42e68fe667fe572
dbarnett <dbarnett@e9a468ab-b372-0410-861a-d954c37f4f16>
2005-05-08 20:41:04 +0000

    Changed calls to deprecated getRootCause()

--- drools/drools-core/src/test/org/drools/spi/ConsequenceExceptionTest.java
+++ drools/drools-core/src/test/org/drools/spi/ConsequenceExceptionTest.java
@@ -1,7 +1,7 @@
 package org.drools.spi;
 
 /*
- * $Id: ConsequenceExceptionTest.java,v 1.3 2005-05-08 04:05:13 dbarnett Exp $
+ * $Id: ConsequenceExceptionTest.java,v 1.4 2005-05-08 20:41:04 dbarnett Exp $
  *
  * Copyright 2003-2005 (C) The Werken Company. All Rights Reserved.
  *
@@ -49,7 +49,7 @@ public class ConsequenceExceptionTest extends DroolsTestCase
     {
         ConsequenceException e = new ConsequenceException( );
 
-        assertNull( e.getRootCause( ) );
+        assertNull( e.getCause( ) );
     }
 
     public void testConstruct_WithRootCause()
@@ -58,7 +58,7 @@ public class ConsequenceExceptionTest extends DroolsTestCase
 
         ConsequenceException e = new ConsequenceException( rootCause );
 
-        assertSame( rootCause, e.getRootCause( ) );
+        assertSame( rootCause, e.getCause( ) );
     }
 
 }
