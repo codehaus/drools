9b259f0f01a016b753e010b179c21727f6db1b22
memelet <memelet@e9a468ab-b372-0410-861a-d954c37f4f16>
2005-05-08 23:43:49 +0000

    DROOLS-351 - Remove drools-annotation and derivitives from 2.1

--- drools/drools-examples-jdk5/src/main/org/drools/spring/examples/jiahvac/model/Floor.java
+++
@@ -1,9 +0,0 @@
-package org.drools.spring.examples.jiahvac.model;
-
-public interface Floor
-{
-    int getNumber();
-    Thermometer getThermometer();
-    Vent getVent();
-    HeatPump getHeatPump();
-}
