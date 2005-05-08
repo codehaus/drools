9b1df7e96e9df972e1b38cfbc55dfd4e22ebaaa8
memelet <memelet@e9a468ab-b372-0410-861a-d954c37f4f16>
2005-05-08 23:43:49 +0000

    DROOLS-351 - Remove drools-annotation and derivitives from 2.1

--- trunk/drools/drools-annotation/src/main/org/drools/semantics/annotation/model/Argument.java
+++
@@ -1,9 +0,0 @@
-package org.drools.semantics.annotation.model;
-
-import java.io.Serializable;
-import org.drools.spi.Tuple;
-
-public interface Argument extends Serializable {
-
-    Object getValue(Tuple tuple);
-}
--- trunk/drools/drools-examples-jdk5/src/main/org/drools/examples/jiahvac/model/Floor.java
+++
@@ -1,9 +0,0 @@
-package org.drools.examples.jiahvac.model;
-
-public interface Floor
-{
-    int getNumber();
-    Thermometer getThermometer();
-    Vent getVent();
-    HeatPump getHeatPump();
-}
