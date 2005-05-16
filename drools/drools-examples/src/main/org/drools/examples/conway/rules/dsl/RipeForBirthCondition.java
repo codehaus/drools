a95086d493cb7a95cda0eb770c54e9ec0dc521c1
brownj <brownj@e9a468ab-b372-0410-861a-d954c37f4f16>
2005-05-16 23:04:31 +0000

    significant refactoring of the conway dsl implementation

---
+++ drools/drools-examples/src/main/org/drools/examples/conway/rules/dsl/RipeForBirthCondition.java
@@ -0,0 +1,51 @@
+package org.drools.examples.conway.rules.dsl;
+
+import org.drools.examples.conway.Cell;
+import org.drools.rule.Declaration;
+import org.drools.spi.Tuple;
+import org.drools.spi.Condition;
+
+/**
+ * @author <a href="mailto:brown_j@ociweb.com">Jeff Brown</a>
+ * @version $Id: RipeForBirthCondition.java,v 1.1.2.1 2005-05-16 23:04:31 brownj Exp $
+ */
+public class RipeForBirthCondition implements Condition
+{
+    protected final Declaration cellDeclaration;
+
+    public RipeForBirthCondition( Declaration cellDeclaration)
+    {
+        this.cellDeclaration = cellDeclaration;
+    }
+
+    /**
+     * Determine if the supplied <code>Tuple</code> is allowed by this
+     * condition.
+     *
+     * @param tuple
+     *            The <code>Tuple</code> to test.
+     *
+     * @return <code>true</code> if the <code>Tuple</code> passes this
+     *         condition, else <code>false</code>.
+     *
+     */
+    public boolean isAllowed(Tuple tuple)
+    {
+        Cell cell = (Cell) tuple.get( cellDeclaration );
+        int numberOfLiveNeighborsCellHas = cell.getNumberOfLiveNeighbors();
+        boolean isAllowed = ( numberOfLiveNeighborsCellHas == 3 );
+        return isAllowed;
+    }
+
+    /**
+     * Retrieve the array of <code>Declaration</code> s required by this
+     * condition to perform its duties.
+     *
+     * @return The array of <code>Declarations</code> expected on incoming
+     *         <code>Tuples</code>.
+     */
+    public Declaration[] getRequiredTupleMembers()
+    {
+        return new Declaration[]{cellDeclaration};
+    }
+}
