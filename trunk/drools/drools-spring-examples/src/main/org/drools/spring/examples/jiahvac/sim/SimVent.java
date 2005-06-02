61798676d6c902f9b106c60adf73a9a447dd5340
memelet <memelet@e9a468ab-b372-0410-861a-d954c37f4f16>
2005-06-02 18:30:47 +0000

    Restoring drools-spring-examples

---
+++ trunk/drools/drools-spring-examples/src/main/org/drools/spring/examples/jiahvac/sim/SimVent.java
@@ -0,0 +1,47 @@
+package org.drools.spring.examples.jiahvac.sim;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+import org.drools.spring.examples.jiahvac.model.Vent;
+
+public class SimVent implements Vent {
+
+    private SimFloor floor;
+    private State state = State.CLOSED;
+    private State prevState = State.CLOSED;
+    private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+
+    public SimVent(SimFloor floor) {
+        this.floor = floor;
+    }
+
+    public SimFloor getFloor() {
+        return floor;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.prevState = this.state;
+        this.state = state;
+        pcs.firePropertyChange("state", prevState, state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        pcs.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        pcs.removePropertyChangeListener(listener);
+    }
+
+    @Override
+    public String toString() {
+        return "vent("+floor.getNumber()+")=" + state;
+    }
+}
+
+
