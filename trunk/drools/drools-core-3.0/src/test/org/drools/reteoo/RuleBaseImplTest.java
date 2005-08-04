fb201fbf160fbf41a9c12a9bee47d067f4ac4bae
mproctor <mproctor@e9a468ab-b372-0410-861a-d954c37f4f16>
2005-08-04 23:33:30 +0000

    -DROOLS-393 We now have an argument to tell the RuleBase not to keep a reference to the WorkingMemory. UnitTests have been updated to reflect this.

--- trunk/drools/drools-core-3.0/src/test/org/drools/reteoo/RuleBaseImplTest.java
+++ trunk/drools/drools-core-3.0/src/test/org/drools/reteoo/RuleBaseImplTest.java
@@ -5,23 +5,36 @@ import org.drools.WorkingMemory;
 
 public class RuleBaseImplTest extends DroolsTestCase
 {
-    public void testWeakReference() throws Exception
+    RuleBaseImpl ruleBase;
+    
+    WorkingMemory wm1;
+    WorkingMemory wm2;
+    WorkingMemory wm3;
+    WorkingMemory wm4;
+    
+    public void setUp()
     {
-        RuleBaseImpl ruleBase = new RuleBaseImpl( new Rete() );
+        this.ruleBase = new RuleBaseImpl( new Rete() );
         
-        WorkingMemory wm1 = ruleBase.newWorkingMemory();
-        WorkingMemory wm2 = ruleBase.newWorkingMemory();
-        WorkingMemory wm3 = ruleBase.newWorkingMemory();
-        WorkingMemory wm4 = ruleBase.newWorkingMemory();
-        
+        this.wm1 = ruleBase.newWorkingMemory();
+        this.wm2 = ruleBase.newWorkingMemory();
+        this.wm3 = ruleBase.newWorkingMemory();
+        this.wm4 = ruleBase.newWorkingMemory();        
+    }
+    
+    public void testKeepReference() throws Exception
+    {
         /* Make sure the RuleBase is referencing all 4 Working Memories */
         assertLength( 4,
                       ruleBase.getWorkingMemories() );        
         assertTrue( ruleBase.getWorkingMemories().contains( wm1 ) );
         assertTrue( ruleBase.getWorkingMemories().contains( wm2 ) );
         assertTrue( ruleBase.getWorkingMemories().contains( wm3 ) );
-        assertTrue( ruleBase.getWorkingMemories().contains( wm4 ) );        
-        
+        assertTrue( ruleBase.getWorkingMemories().contains( wm4 ) );          
+    }
+    
+    public void testWeakReference() throws Exception
+    {                      
         /* nulling these two so the keys should get garbage collected */
         wm2 = null;        
         wm4 = null;
@@ -39,15 +52,30 @@ public class RuleBaseImplTest extends DroolsTestCase
         assertFalse( ruleBase.getWorkingMemories().contains( wm2 ) );
         assertTrue( ruleBase.getWorkingMemories().contains( wm3 ) );
         assertFalse( ruleBase.getWorkingMemories().contains( wm4 ) );
-        
+               
+    }
+    
+    public void testDispose() throws Exception
+    {
         /* Now lets test the dispose method on the WorkingMemory itself.
          * dispose doesn't need GC */
         wm3.dispose();                
                 
-        /* Check only wm1 is left */
-        assertLength( 1,
+        /* Check only wm3 was removed */
+        assertLength( 3,
                       ruleBase.getWorkingMemories() );        
-        assertTrue( ruleBase.getWorkingMemories().contains( wm1 ) );        
+        assertFalse( ruleBase.getWorkingMemories().contains( wm3 ) );          
     }
+    
+    public void testNoKeepReference() throws Exception
+    {
+        WorkingMemory wm5 = this.ruleBase.newWorkingMemory( false );
+        WorkingMemory wm6 = this.ruleBase.newWorkingMemory( false );
+        assertLength( 4,
+                      ruleBase.getWorkingMemories() );          
+        assertFalse( ruleBase.getWorkingMemories().contains( wm5 ) );
+        assertFalse( ruleBase.getWorkingMemories().contains( wm6 ) );
+    }    
+    
 
 }
