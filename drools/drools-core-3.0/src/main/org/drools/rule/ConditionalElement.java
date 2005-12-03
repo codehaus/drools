afba1e68fb1e42daab2c51f944be0660e03ae3ed
mproctor <mproctor@e9a468ab-b372-0410-861a-d954c37f4f16>
2005-12-03 19:26:34 +0000

    DROOLS-464 -Make Patterns cloneable

--- drools/drools-core-3.0/src/main/org/drools/rule/ConditionalElement.java
+++ drools/drools-core-3.0/src/main/org/drools/rule/ConditionalElement.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public abstract class ConditionalElement
+
+public abstract class ConditionalElement implements Cloneable
 {
     private List children = new ArrayList( );    
     
@@ -17,5 +18,43 @@ public abstract class ConditionalElement
     {
         return this.children;
     }            
+
+    /**
+     * Clones all Conditional Elements but references the expressions
+     * 
+     * @param e1
+     * @param e2
+     * @return
+     */
+    public Object clone()
+    {
+        ConditionalElement cloned = null;
+        
+        try
+        {
+            cloned = (ConditionalElement) this.getClass().newInstance();
+        }
+        catch ( InstantiationException e )
+        {
+            throw new RuntimeException("Could not clone '" + this.getClass().getName() + "'" );
+        }
+        catch ( IllegalAccessException e )
+        {
+            throw new RuntimeException("Could not clone '" + this.getClass().getName() + "'" );
+        }
+        
+        for (Iterator it = this.children.iterator(); it.hasNext();)
+        {
+            Object object = it.next();
+            if (object instanceof ConditionalElement)
+            {
+                object = ( (ConditionalElement) object).clone();
+            }
+            cloned.addChild( object );
+                       
+        }
+        
+        return cloned;        
+    }
     
 }
