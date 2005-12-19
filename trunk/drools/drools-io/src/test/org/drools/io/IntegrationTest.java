10d6f46d681f3c298651dbd4882e64b3b7563f2e
michaelneale <michaelneale@e9a468ab-b372-0410-861a-d954c37f4f16>
2005-12-19 02:11:25 +0000

    DROOLS-463 plus additional SMF related fixed, classloader changes and so on to do with the new DDJ feature

--- trunk/drools/drools-io/src/test/org/drools/io/IntegrationTest.java
+++ trunk/drools/drools-io/src/test/org/drools/io/IntegrationTest.java
@@ -1,6 +1,10 @@
 package org.drools.io;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -73,7 +77,39 @@ public class IntegrationTest  extends TestCase
         runTest(ruleSetLoader, map);
         
         assertEquals( new Long( 12586269025L ), map.get("value") );                
-    }       
+    }
+    
+    /** check that the rule base is serializable, via the rulebase proxy */
+    public void testSerializeRuleBase() throws Exception {
+
+        RuleBase rb = RuleBaseLoader.loadFromUrl(IntegrationTest.class.getResource( "fibonacci.mixed.drl" ));
+        assertTrue(rb instanceof SerializableRuleBaseProxy);
+        
+        RuleBase loadedRb = serializeAndBack( rb );
+        assertTrue(loadedRb instanceof SerializableRuleBaseProxy);
+        
+        RuleBase loadedRb2 = serializeAndBack( loadedRb );
+        assertTrue(loadedRb2 instanceof SerializableRuleBaseProxy);            
+        
+    }
+
+    private RuleBase serializeAndBack(RuleBase rb) throws IOException,
+                                                  ClassNotFoundException
+    {
+        ByteArrayOutputStream outByte = new ByteArrayOutputStream();
+        
+        
+        ObjectOutputStream objStream = new ObjectOutputStream(outByte);
+        objStream.writeObject(rb);
+        objStream.flush();
+        objStream.close();
+        
+        ByteArrayInputStream inByte = new ByteArrayInputStream(outByte.toByteArray());
+         
+        ObjectInputStream objIn = new ObjectInputStream(inByte);
+        RuleBase loadedRb = (RuleBase) objIn.readObject();
+        return loadedRb;
+    }
     
     private void runTest(RuleSetLoader ruleSetLoader, Map map) throws IntegrationException, SAXException, IOException, FactException
     {
