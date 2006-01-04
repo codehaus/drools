e9111b19d5d2b647d9e29f722915940f6813621f
krisv <krisv@e9a468ab-b372-0410-861a-d954c37f4f16>
2006-01-04 13:20:49 +0000

    When parsing rules, the classpath of the project the drl file is located in is included (classpath of required projects not (yet)).

---
+++ trunk/drools/drools-ide/src/main/org/drools/ide/util/ProjectClassLoader.java
@@ -0,0 +1,77 @@
+package org.drools.ide.util;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.drools.ide.DroolsIDEPlugin;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaModelException;
+
+public class ProjectClassLoader {
+
+    public static URLClassLoader getProjectClassLoader(IJavaProject project) {
+        List<URL> pathElements = getProjectClassPathURLs(project);
+        URL urlPaths[] = pathElements.toArray(new URL[pathElements.size()]);
+        return new URLClassLoader(urlPaths, Thread.currentThread().getContextClassLoader());
+    }
+
+    private static URL getRawLocationURL(IPath simplePath)
+            throws MalformedURLException {
+        File file = getRawLocationFile(simplePath);
+        return file.toURL();
+    }
+
+    private static File getRawLocationFile(IPath simplePath) {
+        IResource resource = ResourcesPlugin.getWorkspace().getRoot().findMember(simplePath);
+        File file = null;
+        if (resource != null) {
+            file = ResourcesPlugin.getWorkspace().getRoot().findMember(
+                    simplePath).getRawLocation().toFile();
+        } else {
+            file = simplePath.toFile();
+        }
+        return file;
+    }
+
+    public static List<URL> getProjectClassPathURLs(IJavaProject project) {
+        List<URL> pathElements = new ArrayList<URL>();
+        try {
+            IClasspathEntry[] paths = project.getResolvedClasspath(true);
+            if (paths != null) {
+                for (IClasspathEntry path: paths) {
+                    if (path.getEntryKind() == IClasspathEntry.CPE_LIBRARY) {
+                        URL url = getRawLocationURL(path.getPath());
+                        pathElements.add(url);
+                    }
+                }
+            }
+            IPath location = getProjectLocation(project.getProject());
+            IPath outputPath = location.append(project.getOutputLocation()
+                    .removeFirstSegments(1));
+            pathElements.add(outputPath.toFile().toURL());
+            // TODO: is it possible to add classpath of required projects too?
+        } catch (JavaModelException e) {
+            DroolsIDEPlugin.log(e);
+        } catch (MalformedURLException e) {
+            DroolsIDEPlugin.log(e);
+        }
+        return pathElements;
+    }
+    
+    public static IPath getProjectLocation(IProject project) {
+        if (project.getRawLocation() == null) {
+            return project.getLocation();
+        } else {
+            return project.getRawLocation();
+        }
+    }
+}
