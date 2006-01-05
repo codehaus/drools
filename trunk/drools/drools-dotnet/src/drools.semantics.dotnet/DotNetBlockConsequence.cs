76483f8a1d48993266f55ad44f8f586580d12c95
mmiller678 <mmiller678@e9a468ab-b372-0410-861a-d954c37f4f16>
2006-01-05 08:08:13 +0000

    .NET Semantics module for Drools

---
+++ trunk/drools/drools-dotnet/src/drools.semantics.dotnet/DotNetBlockConsequence.cs
@@ -0,0 +1,75 @@
+using org.drools.spi;
+using org.drools.rule;
+using System.Reflection;
+using System.CodeDom;
+using Microsoft.CSharp;
+using System.Collections.Generic;
+using System;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Text;
+
+namespace org.drools.semantics.dotnet
+{
+	public class DotNetBlockConsequence : Consequence
+	{
+		private int _id;
+		private Rule _rule;
+		private string _expression;
+		private Assembly _assembly;
+		private string _className;
+		private string _methodName = "Invoke";
+
+		public DotNetBlockConsequence(Rule rule, int id, string expression)
+		{
+			_id = id;
+			_rule = rule;
+			_expression = expression;
+			_className = "Consequence_" + id;
+			_assembly = Compile();
+		}
+
+		public Assembly Assembly
+		{
+			get { return _assembly; }
+		}
+
+		public void invoke(Tuple t)
+		{
+			try
+			{
+				List<object> parameters = new List<object>();
+				foreach (Declaration d in _rule.getParameterDeclarations().toArray(new Declaration[] { }))
+				{
+					parameters.Add(t.get(d));
+				}
+				parameters.Add(new DefaultKnowledgeHelper(_rule, t));
+				object o = _assembly.CreateInstance(this.GetType().Namespace + "." + _className);
+				if (o == null) throw new ConsequenceException("Unable to find class: " + _className + " in " + this.ToString(), _rule);
+				o.GetType().GetMethod(_methodName).Invoke(o, parameters.ToArray());
+			}
+			catch (Exception e)
+			{
+				throw new ConsequenceException(e.GetBaseException(), _rule, "Error executing " + this.ToString());
+			}
+		}
+
+		private Assembly Compile()
+		{
+			//Generate Code
+			CodeCompileUnit code = CodeGenerator.CreateConsequence(this.GetType().Namespace,
+				_className, _methodName, (Declaration[]) 
+				_rule.getParameterDeclarations().toArray(new Declaration[]{}),
+				_expression, (DotNetImporter)_rule.getImporter(),
+				(DotNetFunctions)_rule.getRuleSet().getFunctions("dotnet"));
+
+			//Generate IL
+			return CodeCompiler.Compile(code);
+		}
+
+		public override string ToString()
+		{
+			return "[Consequence: " + _expression + "]";
+		}
+	}
+}
---
+++ trunk/drools/drools-dotnet/src/drools.semantics.dotnet/DotNetFunctionsFactory.cs
@@ -0,0 +1,25 @@
+using System;
+using org.drools.rule;
+using org.drools.smf;
+using org.drools.spi;
+
+namespace org.drools.semantics.dotnet
+{
+	/// <summary>
+	/// .NET Function Factory
+	/// </summary>
+	public class DotNetFunctionsFactory : FunctionsFactory
+	{
+		public Functions newFunctions(RuleSet rs, RuleBaseContext rbc, Configuration c)
+		{
+			try
+			{
+				return new DotNetFunctions(c.getText());
+			}
+			catch (Exception e)
+			{
+				throw new FactoryException(e);
+			}
+		}
+	}
+}
