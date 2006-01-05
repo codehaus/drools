faedb9bfb6e9e8aaf978b80bae19a10f61a4a10c
mmiller678 <mmiller678@e9a468ab-b372-0410-861a-d954c37f4f16>
2006-01-05 08:07:29 +0000

    .NET unit tests for Drools

---
+++ trunk/drools/drools-dotnet/src/drools.dotnet.tests/semantics/DotNetLanguages.cs
@@ -0,0 +1,56 @@
+using System;
+using NUnit.Framework;
+using System.Configuration;
+
+namespace org.drools.dotnet.tests.semantics
+{
+	[TestFixture]
+	public class DotNetLanguages
+	{
+		private string settingName = "drools.dotnet.codedomprovider";
+
+		[Test]
+		public void TestDefault()
+		{
+			string providerName = null;
+			SetProvider(providerName);
+			Assert.AreEqual(providerName, ConfigurationManager.AppSettings[settingName]);
+			RuleBase rulebase = RuleBaseLoader.LoadFromUri(
+				new Uri("./drls/csharp.drl.xml", UriKind.Relative));
+		}
+
+		[Test]
+		public void TestVisualBasic()
+		{
+			string providerName = "Microsoft.VisualBasic.VBCodeProvider, System, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089";
+			SetProvider(providerName);
+			Assert.AreEqual(providerName, ConfigurationManager.AppSettings[settingName]);
+			RuleBase rulebase = RuleBaseLoader.LoadFromUri(
+				new Uri("./drls/visualbasic.drl.xml", UriKind.Relative));
+		}
+
+		[Test]
+		public void TestCSharp()
+		{
+			string providerName = "Microsoft.CSharp.CSharpCodeProvider, System, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089";
+			SetProvider(providerName);
+			Assert.AreEqual(providerName, ConfigurationManager.AppSettings[settingName]);
+			RuleBase rulebase = RuleBaseLoader.LoadFromUri(
+				new Uri("./drls/csharp.drl.xml", UriKind.Relative));
+		}
+
+		private void SetProvider(string provider)
+		{
+			Configuration config = ConfigurationManager.OpenExeConfiguration(
+				ConfigurationUserLevel.None);
+			config.AppSettings.Settings.Clear();
+			if (provider != null && provider.Length > 0)
+			{
+				config.AppSettings.Settings.Add(settingName,
+					provider);
+			}
+			config.Save(ConfigurationSaveMode.Modified);
+			ConfigurationManager.RefreshSection("appSettings");
+		}
+	}
+}
---
+++ trunk/drools/drools-dotnet/src/drools.dotnet.tests/semantics/DotNetObjectTypeTests.cs
@@ -0,0 +1,71 @@
+using System;
+using NUnit.Framework;
+using org.drools.rule;
+using org.drools.semantics.@base;
+using org.drools.semantics.dotnet;
+using org.drools.smf;
+using org.drools.spi;
+
+namespace org.drools.dotnet.tests.semantics
+{
+    [TestFixture]
+    public class DotNetObjectTypeTests
+    {
+        [Test]
+        [ExpectedException(typeof(FactoryException), "No type name specified.")]
+        public void TestWithMissingType()
+        {
+            RuleBaseContext ruleBaseContext = new RuleBaseContext();
+            DefaultConfiguration configuration = new DefaultConfiguration("test1");
+            RuleSet ruleSet = new RuleSet("test RuleSet", ruleBaseContext);
+            Rule rule = new Rule("Test Rule 1", ruleSet);;
+
+            Importer importer = new DotNetImporter();
+            rule.setImporter(importer);
+
+            ObjectTypeFactory factory = new DotNetObjectTypeFactory();
+            ObjectType type = factory.newObjectType(rule, ruleBaseContext, configuration);
+        }
+
+        [Test]
+        public void TestWithoutImports()
+        {
+            RuleBaseContext ruleBaseContext = new RuleBaseContext();
+            DefaultConfiguration configuration = new DefaultConfiguration("test1");
+            configuration.setText("System.Collections.Hashtable");
+
+            RuleSet ruleSet = new RuleSet("test RuleSet", ruleBaseContext);
+            Rule rule = new Rule("Test Rule 1", ruleSet);
+
+			Importer importer = new DotNetImporter();
+            rule.setImporter(importer);
+
+			ObjectTypeFactory factory = new DotNetObjectTypeFactory();
+			DotNetObjectType type = (DotNetObjectType) 
+				factory.newObjectType(rule, ruleBaseContext, configuration);
+            Assert.AreEqual(configuration.getText(), type.Type.FullName);
+			Assert.IsTrue(type.matches(Activator.CreateInstance(Type.GetType(configuration.getText()))));
+        }
+
+		[Test]
+		public void TestWithImports()
+		{
+			RuleBaseContext ruleBaseContext = new RuleBaseContext();
+			DefaultConfiguration configuration = new DefaultConfiguration("test1");
+			configuration.setText("Hashtable");
+
+			RuleSet ruleSet = new RuleSet("test RuleSet", ruleBaseContext);
+			Rule rule = new Rule("Test Rule 1", ruleSet);
+
+			Importer importer = new DotNetImporter();
+			importer.addImport(new BaseImportEntry("System.Collections"));
+			rule.setImporter(importer);
+
+			ObjectTypeFactory factory = new DotNetObjectTypeFactory();
+			DotNetObjectType type = (DotNetObjectType) 
+				factory.newObjectType(rule, ruleBaseContext, configuration);
+			Assert.AreEqual("System.Collections.Hashtable", type.Type.FullName);
+			Assert.IsTrue(type.matches(Activator.CreateInstance(Type.GetType("System.Collections.Hashtable"))));
+		}
+    }
+}
