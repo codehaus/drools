509a7431048059cf36fbd73fc181839a27840425
michaelneale <michaelneale@e9a468ab-b372-0410-861a-d954c37f4f16>
2006-01-16 01:31:23 +0000

    DROOLS-472 improvements from John Gilbert

--- trunk/drools/drools-jsr94/src/main/org/drools/jsr94/rules/RuleServiceProviderImpl.java
+++ trunk/drools/drools-jsr94/src/main/org/drools/jsr94/rules/RuleServiceProviderImpl.java
@@ -1,7 +1,7 @@
 package org.drools.jsr94.rules;
 
 /*
- * $Id: RuleServiceProviderImpl.java,v 1.11 2004-11-27 00:59:54 dbarnett Exp $
+ * $Id: RuleServiceProviderImpl.java,v 1.12 2006-01-16 01:31:22 michaelneale Exp $
  *
  * Copyright 2002-2004 (C) The Werken Company. All Rights Reserved.
  *
@@ -46,70 +46,76 @@ import javax.rules.RuleServiceProvider;
 import javax.rules.admin.RuleAdministrator;
 
 import org.drools.jsr94.rules.admin.RuleAdministratorImpl;
+import org.drools.jsr94.rules.admin.RuleExecutionSetRepository;
 
 /**
  * This class provides access to the <code>RuleRuntime</code> and
- * <code>RuleAdministrator</code> implementation supplied by Drools
- * when running under J2SE.
- * <p/>
- * This class should be used in environments without a JNDI provider -
- * typically when writing standalone J2SE clients. Within the J2EE environment
- * the <code>RuleServiceProvider</code> implementation class provided by Drools
- * should be retrieved using a JNDI lookup.
- * <p/>
- * This class should be constructed using the
+ * <code>RuleAdministrator</code> implementation supplied by Drools when
+ * running under J2SE. <p/> This class should be used in environments without a
+ * JNDI provider - typically when writing standalone J2SE clients. Within the
+ * J2EE environment the <code>RuleServiceProvider</code> implementation class
+ * provided by Drools should be retrieved using a JNDI lookup. <p/> This class
+ * should be constructed using the
  * <code>RuleServiceProviderManager.getRuleServiceProvider</code> method.
- *
+ * 
  * @see RuleRuntimeImpl
  * @see RuleAdministratorImpl
  * @see RuleServiceProvider
  * @see javax.rules.RuleServiceProviderManager#getRuleServiceProvider(String)
- *
+ * 
  * @author <a href="mailto:thomas.diesler@softcon-itec.de">thomas diesler </a>
  */
-public class RuleServiceProviderImpl extends RuleServiceProvider
-{
-    /** An instance of <code>RuleRuntimeImpl</code>. */
-    private RuleRuntime ruleRuntime;
+public class RuleServiceProviderImpl extends RuleServiceProvider {
+	/** An instance of <code>RuleRuntimeImpl</code>. */
+	private RuleRuntime ruleRuntime;
 
-    /** An instance of <code>RuleAdministratorImpl</code>. */
-    private RuleAdministrator ruleAdministrator;
+	/** An instance of <code>RuleAdministratorImpl</code>. */
+	private RuleAdministrator ruleAdministrator;
 
-    /**
-     * Create a new <code>RuleServiceProviderImpl</code>.
-     */
-    public RuleServiceProviderImpl( )
-    {
-        // no special initialization required
-    }
+	private RuleExecutionSetRepository repository;
 
-    /**
-     * Returns a class instance of <code>RuleRuntime</code>. Specifically
-     * an instance of the Drools <code>RuleRuntimeImpl</code> is returned.
-     *
-     * @return an instance of <code>RuleRuntime</code>
-     */
-    public synchronized RuleRuntime getRuleRuntime( )
-    {
-        if ( this.ruleRuntime != null )
-        {
-            return this.ruleRuntime;
-        }
-        return this.ruleRuntime = new RuleRuntimeImpl( );
-    }
+	/**
+	 * Create a new <code>RuleServiceProviderImpl</code>.
+	 */
+	public RuleServiceProviderImpl() {
+		// no special initialization required
+	}
 
-    /**
-     * Returns a class instance of <code>RuleAdministrator</code>. Specifically
-     * an instance of the Drools <code>RuleAdministratorImpl</code> is returned.
-     *
-     * @return an instance of <code>RuleAdministrator</code>
-     */
-    public synchronized RuleAdministrator getRuleAdministrator( )
-    {
-        if ( this.ruleAdministrator != null )
-        {
-            return this.ruleAdministrator;
-        }
-        return this.ruleAdministrator = new RuleAdministratorImpl( );
-    }
+	/**
+	 * @return
+	 */
+	public synchronized RuleExecutionSetRepository getRepository() {
+		if (this.repository != null) {
+			return this.repository;
+		}
+		return this.repository = new RuleExecutionSetRepository();
+	}
+
+
+	/**
+	 * Returns a class instance of <code>RuleRuntime</code>. Specifically an
+	 * instance of the Drools <code>RuleRuntimeImpl</code> is returned.
+	 * 
+	 * @return an instance of <code>RuleRuntime</code>
+	 */
+	public synchronized RuleRuntime getRuleRuntime() {
+		if (this.ruleRuntime != null) {
+			return this.ruleRuntime;
+		}
+		return this.ruleRuntime = new RuleRuntimeImpl(getRepository());
+	}
+
+	/**
+	 * Returns a class instance of <code>RuleAdministrator</code>.
+	 * Specifically an instance of the Drools <code>RuleAdministratorImpl</code>
+	 * is returned.
+	 * 
+	 * @return an instance of <code>RuleAdministrator</code>
+	 */
+	public synchronized RuleAdministrator getRuleAdministrator() {
+		if (this.ruleAdministrator != null) {
+			return this.ruleAdministrator;
+		}
+		return this.ruleAdministrator = new RuleAdministratorImpl(getRepository());
+	}
 }
