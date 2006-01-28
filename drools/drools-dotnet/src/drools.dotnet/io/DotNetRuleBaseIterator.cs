2e41506a3514f98e27ecd96969facc7f15d3c4db
nagarkar <nagarkar@e9a468ab-b372-0410-861a-d954c37f4f16>
2006-01-28 07:23:51 +0000

    New files for Precompilation support

---
+++ drools/drools-dotnet/src/drools.dotnet/io/DotNetRuleBaseIterator.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using org.drools;
+using java.util;
+using org.drools.rule;
+using System.Reflection;
+using org.drools.util;
+using org.drools.dotnet.util;
+
+namespace org.drools.semantics.dotnet
+{
+    public enum DotNetRuleBaseActions
+    {
+        COMPILE,
+        SETSCRIPT
+    }
+    public class DotNetRuleBaseIterator
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rb"></param>
+        /// <param name="action"></param>
+        /// <returns>An array list that contains additional calculated values. The values depend on the action.
+        /// If action is 'Compile' the array list is a list of assembly names</returns>
+        public static System.Collections.ArrayList Action(DotNetRuleBaseActions action, 
+            System.Collections.Hashtable parameters)
+        {
+            ReadOnlyList<RuleSet> rsarray = parameters["RuleSet"] as ReadOnlyList<RuleSet>;            
+            System.Collections.ArrayList assemblyList = new System.Collections.ArrayList();
+            System.Collections.IEnumerator rsArrayEnum = rsarray.GetEnumerator();
+            while(rsArrayEnum.MoveNext())
+            {
+                RuleSet rs = rsArrayEnum.Current as RuleSet;
+                foreach (Rule r in rs.getRules())
+                {
+                    for (Iterator condit = r.getConditions().iterator(); condit.hasNext(); )
+                    {
+                        DotNetCondition cond = condit.next() as DotNetCondition;
+                        switch (action)
+                        {
+                            case DotNetRuleBaseActions.COMPILE:
+                                {
+                                    Assembly assembly = cond.Compile();
+                                    assemblyList.Add(assembly.ManifestModule.Name);
+
+                                }
+                                break;
+                            case DotNetRuleBaseActions.SETSCRIPT:
+                                {
+
+                                    cond.ResetAssembly(parameters["Assembly"] as Assembly);
+                                }
+                                break;
+                            default: break;
+                        }
+
+                    }
+                    {
+                        DotNetBlockConsequence conseq = r.getConsequence() as DotNetBlockConsequence;
+                        switch (action)
+                        {
+                            case DotNetRuleBaseActions.COMPILE:
+                                {
+                                    Assembly assembly = conseq.Compile();
+                                    assemblyList.Add(assembly.ManifestModule.Name);
+                                }
+                                break;
+                            case DotNetRuleBaseActions.SETSCRIPT:
+                                {
+                                    conseq.ResetAssembly(parameters["Assembly"] as Assembly);
+                                }
+                                break;
+                            default: break;
+                        }
+                    }
+                }
+            }
+            return assemblyList;
+        }
+        
+    }
+}
